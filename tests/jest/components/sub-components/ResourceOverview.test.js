019443e6745eded32edee206960c88cfce2ba788
Xuning Kang <Xuning.Kang@ibm.com>
2018-08-02 11:36:41 -0400

    Added few test files (#170)
    * Added few test files
    
    * Add snapshot
    
    * Modified one test file
    
    * Revert "Modified one test file"
    
    This reverts commit a69dc0b0691c4a3b7a00a4b5db830025cf291c1e.
    
    * Removed used test toolkit

---
+++ tests/jest/components/sub-components/ResourceOverview.test.js
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2018. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
+'use strict'
+
+import React from 'react'
+import renderer from 'react-test-renderer'
+
+import ResourceOverview from '../../../../src-web/components/dashboard/ResourceOverview'
+
+describe('ResourceOverview no components', () => {
+  it('renders as expected', () => {
+    const component = renderer.create(
+      <ResourceOverview />
+    )
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+})
+
+const mockItem = {
+  critical:1,
+  warning:2,
+  healthy:3,
+  name:'4',
+  table:[[1,2],[3,4]]
+}
+
+describe('ResourceOverview no components', () => {
+  it('renders as expected', () => {
+    const component = renderer.create(
+      <ResourceOverview cardItem={mockItem} />
+    )
+    expect(component.toJSON()).toMatchSnapshot()
+  })
+})

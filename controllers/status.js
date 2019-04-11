ee3ac283a1ea87f55bcb2040ef397f61ce375bd1
Alec Hsu <Alec.Hsu@ibm.com>
2019-04-11 15:49:37 -0400

    Add readiness probe check (#522)
    * add iam and header call
    
    * fix headers undefined
    
    * add mcm ui api call
    
    * copyright
    
    * remove redundant and send status code properly refer to https://github.ibm.com/IBMPrivateCloud/platform-ui/pull/1235
    
    * revert endpoint

--- controllers/status.js
+++ controllers/status.js
@@ -11,14 +11,17 @@
 var express = require('express'),
     router = express.Router(),
     log4js = require('log4js'),
-    logger = log4js.getLogger('status')
+    logger = log4js.getLogger('status'),
+    async = require('async'),
+    iamClient = require('../lib/server/iam-client'),
+    headerClient = require('../lib/server/header-client')
 
 router.get('/', (req, res) => {
   logger.debug('/')
   res.sendStatus(200)
 })
 
-router.get('/status', (req, res) => res.sendStatus(200) )
+router.get('/status', (req, res) => res.sendStatus(200))
 
 router.get('/livenessProbe', (req, res) => {
   logger.debug('/livenessProbe')
@@ -26,8 +29,21 @@ router.get('/livenessProbe', (req, res) => {
 })
 
 router.get('/readinessProbe', (req, res) => {
-  logger.debug('/readinessProbe')
-  res.send(`Testing readinessProbe --> ${new Date().toLocaleString()}`)
+  const iamPlatformIdentity = iamClient.getPlatformIdentity.bind(iamClient, req)
+  const header = headerClient.checkStatus.bind(headerClient, req)
+  const calls = [iamPlatformIdentity, header]
+  async.parallel(
+    calls, (err) => {
+      let statusCode
+      if (err) {
+        logger.error(`GET ${req.path} ${err.message} ${err.statusCode || 500}`)
+        statusCode = err.statusCode
+      } else {
+        logger.debug(req.path)
+        statusCode = res.statusCode
+      }
+      return res.sendStatus(statusCode >= 100 && statusCode < 600 ? statusCode : 500)
+    })
 })
 
 module.exports = router

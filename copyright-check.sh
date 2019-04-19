176e08d004cdd8c3de654f9a1bb171e249576dd9
Aniruddh Jhavar <Aniruddh.Jhavar@ibm.com>
2019-04-19 12:33:35 -0400

    update copyright in headers (#556)

--- copyright-check.sh
+++ copyright-check.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Licensed Materials - Property of IBM
-# (c) Copyright IBM Corporation 2018. All Rights Reserved.
+# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
 # Note to U.S. Government Users Restricted Rights:
 # Use, duplication or disclosure restricted by GSA ADP Schedule
 # Contract with IBM Corp.

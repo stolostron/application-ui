a91ce6edf21b5b93a1a70a78d756bcf65a07fc7b
Zack B. Layne <Zack.Layne1@ibm.com>
2019-04-24 14:19:27 -0400

    Add update functionality to the YAML tab (#564)
    * Add update functionality to the YAML tab
    
    * update button text
    
    * Review edits
    
    * use vh instead of rem
    
    * Review edits

--- src-web/components/details/YAMLTab.js
+++ src-web/components/details/YAMLTab.js
@@ -8,46 +8,119 @@
  *******************************************************************************/
 'use strict'
 
+import _ from 'lodash'
 import React from 'react'
 import PropTypes from 'prop-types'
-import _ from 'lodash'
+import jsYaml from 'js-yaml'
+import msgs from '../../../nls/platform.properties'
 import YamlEditor from '../common/YamlEditor'
-import { toString } from '../../../lib/client/design-helper'
-import { Loading, Notification } from 'carbon-components-react'
+import { saveLoad } from '../../../lib/client/design-helper'
+import { Button, Loading, Modal, Notification } from 'carbon-components-react'
+import apolloClient from '../../../lib/client/apollo-client'
+import { canCallAction } from '../../../lib/client/access-helper'
 
 class YAMLTab extends React.Component {
-
-  static propTypes = {
-    resourceJson: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
-    resourceLoading: PropTypes.bool
-  }
-
   constructor(props) {
     super(props)
+    this.client = apolloClient.getClient()
     this.state = {
-      readOnly: true,
-      // yamlParsingError: null,
+      readOnly: true, // TODO - check RBAC for editing
+      open: false,
+      loading: false,
     }
   }
 
-  render() {
-    const { resourceJson, resourceLoading } = this.props
+  formatData(data) {
+    this.setState({
+      resourceJson: jsYaml.safeDump(data)
+    })
+  }
 
-    if (resourceLoading)
+  componentDidUpdate(prevProps) {
+    if (prevProps.resourceJson !== this.props.resourceJson) {
+      this.formatData(this.props.resourceJson)
+      canCallAction(this.props.kind, 'update').then(response => {
+        if (_.get(response, 'data.userAccess.allowed')) {
+          this.setState({
+            readOnly: false
+          })
+        }
+      })
+    }
+  }
+
+  handleEditorChange = value => {
+    this.setState({ resourceJson: value })
+  }
+
+  toggleModal() {
+    this.setState((prevState) => ({ open: !prevState.open }))
+  }
+
+  handleSubmit() {
+    const { selfLink, namespace, kind, name, cluster } = this.props
+    const { resourceJson } = this.state
+    const body = saveLoad(resourceJson)
+    this.setState({
+      loading: true
+    })
+    apolloClient.genericUpdate({ selfLink, namespace, kind, name, body: body[0], cluster }).then((res) => {
+      this.setState({
+        errors: res.errors ? res.errors[0].message : null,
+        loading: false
+      })
+      this.toggleModal()
+    })
+  }
+
+  renderConfirmationModal() {
+    const { open } = this.state
+    const { locale } = this.context
+    const { name } = this.props
+    return (
+      <Modal
+        danger
+        id='update-resource-modal'
+        open={open}
+        primaryButtonText={msgs.get('actions.save', locale)}
+        secondaryButtonText={msgs.get('modal.button.cancel', locale)}
+        modalLabel={name}
+        modalHeading={msgs.get('modal.edit.resource.header', locale)}
+        onRequestClose={this.toggleModal.bind(this)}
+        onRequestSubmit={this.handleSubmit.bind(this)}
+        role='region'
+        aria-label={msgs.get('modal.edit.resource.header', locale)}>
+        {msgs.get('modal.edit.resource.confirmation', locale)}
+      </Modal>
+    )
+  }
+
+  render() {
+    const { resourceLoading } = this.props
+    const { readOnly, resourceJson, errors, loading } = this.state
+
+    if (resourceLoading || loading)
       return <Loading className='content-spinner' />
 
     if (resourceJson && _.get(resourceJson, '[0].message'))
       return <Notification kind='error' title='' subtitle={resourceJson[0].message} />
 
-    if (resourceJson && resourceJson) {
+    if (resourceJson) {
       return (
-        <div className='details-yaml'>
-          <YamlEditor
-            width='100%'
-            height='500px'
-            readOnly={this.state.readOnly}
-            onYamlChange={this.handleEditorChange}
-            yaml={toString({raw: resourceJson})} />
+        <div >
+          {errors ? <Notification kind='error' title='' subtitle={errors} /> : null}
+          <div className={'details-yaml-editButton'}>
+            <Button onClick={this.toggleModal.bind(this)}>{msgs.get('actions.save', this.context.locale)}</Button>
+          </div>
+          <div className='details-yaml'>
+            <YamlEditor
+              width='100%'
+              height='65vh'
+              readOnly={readOnly}
+              onYamlChange={this.handleEditorChange}
+              yaml={resourceJson} />
+          </div>
+          {this.renderConfirmationModal()}
         </div>
       )
     }
@@ -58,6 +131,16 @@ class YAMLTab extends React.Component {
 
 YAMLTab.contextTypes = {
   locale: PropTypes.string
+}
+
+YAMLTab.propTypes = {
+  cluster: PropTypes.string,
+  kind: PropTypes.string,
+  name: PropTypes.string,
+  namespace: PropTypes.string,
+  resourceJson: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+  resourceLoading: PropTypes.bool,
+  selfLink: PropTypes.string,
 }
 
 export default YAMLTab

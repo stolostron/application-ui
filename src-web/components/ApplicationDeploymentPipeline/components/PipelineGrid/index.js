c90d864ca0bcbbb4355b782d8f3249a38d0261de
Josh D. King <jdking@us.ibm.com>
2019-07-02 14:00:05 -0500

    the table is more ironed out a bit ... but much more to come

--- src-web/components/ApplicationDeploymentPipeline/components/PipelineGrid/index.js
+++ src-web/components/ApplicationDeploymentPipeline/components/PipelineGrid/index.js
@@ -8,16 +8,37 @@
  *******************************************************************************/
 
 import React from 'react';
-import { Tile } from 'carbon-components-react';
 import msgs from '../../../../../nls/platform.properties';
 import { withLocale } from '../../../../providers/LocaleProvider';
 import resources from '../../../../../lib/shared/resources';
+import { createApplicationRows, createApplicationRowsLookUp } from './utils';
+import { Tile, DataTable } from 'carbon-components-react';
+
+const {
+  TableBody,
+  Table,
+  TableExpandRow,
+  TableCell,
+  TableExpandedRow,
+} = DataTable;
 
 resources(() => {
   require('./style.scss');
 });
 
+// We would have a headers array like the following
+const headerss = [
+  {
+    // `key` is the name of the field on the row object itself for the header
+    key: 'name',
+    // `header` will be the name you want rendered in the Table Header
+    header: 'Foo',
+  },
+];
+
 const PipelineGrid = withLocale(({ deployables, applications, locale }) => {
+  const applicationRows = createApplicationRows(applications);
+  const applicationRowsLookUp = createApplicationRowsLookUp(applications);
   return (
     <div id="PipelineGrid">
       <div className="tileContainer">
@@ -32,6 +53,42 @@ const PipelineGrid = withLocale(({ deployables, applications, locale }) => {
           </div>
         </Tile>
       </div>
+      <DataTable
+        headers={headerss}
+        rows={applicationRows}
+        render={({ rows, headers, getRowProps, getTableProps }) => (
+          <Table {...getTableProps()}>
+            <TableBody>
+              {rows.map((row) => {
+                const thisRowId = row.id;
+                const deployablesList =
+                  applicationRowsLookUp[thisRowId].deployables || [];
+                return (
+                  <React.Fragment key={thisRowId}>
+                    <TableExpandRow {...getRowProps({ row })}>
+                      {row.cells.map(cell => (
+                        <TableCell key={cell.id} className="tableCell">
+                          <div className="applicationEntryName">
+                            {cell.value}
+                          </div>
+                          <div>{`${deployablesList.length} deployables`}</div>
+                        </TableCell>
+                      ))}
+                    </TableExpandRow>
+                    {/* toggle based off of if the row is expanded. If it is, render TableExpandedRow */}
+                    {row.isExpanded && (
+                      <TableExpandedRow colSpan={headers.length + 1}>
+                        <h1>Expandable row content</h1>
+                        <p>Description here</p>
+                      </TableExpandedRow>
+                    )}
+                  </React.Fragment>
+                );
+              })}
+            </TableBody>
+          </Table>
+        )}
+      />
     </div>
   );
 });
---
+++ src-web/components/ApplicationDeploymentPipeline/components/PipelineGrid/utils.js
@@ -0,0 +1,48 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2017, 2019. All Rights Reserved.
+ *
+ * Note to U.S. Government Users Restricted Rights:
+ * Use, duplication or disclosure restricted by GSA ADP Schedule
+ * Contract with IBM Corp.
+ *******************************************************************************/
+
+import R from 'ramda';
+
+// A created Mapper to create the row for our application data table
+const mapApplicationLookUp = (application) => {
+  const { metadata, deployables } = application;
+  const idRef = (metadata && metadata.name) || 'default';
+  return {
+    [idRef]: {
+      id: (metadata && metadata.name) || '',
+      name: (metadata && metadata.name) || '',
+      deployables: deployables || [],
+    },
+  };
+};
+
+// A created Mapper to create the row for our application data table
+const mapApplicationForRow = (application) => {
+  const { metadata, deployables } = application;
+  return {
+    id: (metadata && metadata.name) || '',
+    name: (metadata && metadata.name) || '',
+    deployables: deployables || [],
+  };
+};
+
+// Method will take in an object of applications and return back a mapped version
+// for the DataTable
+export const createApplicationRows = (list) => {
+  const mappedApps = list.map(item => mapApplicationForRow(item));
+  return mappedApps;
+};
+
+// Method will take in an object of applications and return back a mapped version
+// for the DataTable that will contain more data that we will use to look up and
+// reference given the ID
+export const createApplicationRowsLookUp = (list) => {
+  const mappedApps = list.map(item => mapApplicationLookUp(item));
+  return R.mergeAll(mappedApps);
+};

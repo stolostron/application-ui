f9e3f2aa4a351622a29e36b296f0926b6e328a1e
Lin Song <lin.song@ibm.com>
2019-07-03 12:15:02 -0500

    Cleanup

--- src-web/components/common/CountsCardModule.js
+++ src-web/components/common/CountsCardModule.js
@@ -13,25 +13,33 @@ import PropTypes from 'prop-types'
 import classNames from 'classnames'
 import resources from '../../../lib/shared/resources'
 import msgs from '../../../nls/platform.properties'
-import _ from 'lodash'
 
 resources(() => {
   require('../../../scss/counts-card-module.scss')
 })
 
 export default class CountsCardModule extends React.Component {
+  getModuleData = () => {
+    const { locale } = this.context
+    return {
+      countCardItems: [
+        { count: 6, type: msgs.get('table.header.deployables', locale) },
+        { count: 8, type: msgs.get('table.header.deployments', locale) },
+        { count: 2, type: msgs.get('table.header.failedDeployments', locale) },
+      ]
+    }
+  }
 
   render() {
     const { locale } = this.context
-    const { handleDrillDownClick } = this.props
     const moduleData = this.getModuleData()
     return (
-      <div className='counts-card-module'>
-        <div className='card-container-container'>
-          <div className='card-container'>
-            <div className='card-content'>
-              <div className='card-inner-content'>
-                <CountCards moduleData={moduleData} handleClick={handleDrillDownClick} locale={locale} />
+      <div className="counts-card-module">
+        <div className="card-container-container">
+          <div className="card-container">
+            <div className="card-content">
+              <div className="card-inner-content">
+                <CountCards moduleData={moduleData} locale={locale} />
               </div>
             </div>
           </div>
@@ -39,46 +47,39 @@ export default class CountsCardModule extends React.Component {
       </div>
     )
   }
-
-  getModuleData = () => {
-    const { locale } = this.context
-    return {
-      countCardItems: [
-        {count: 6, type: msgs.get('table.header.deployables', locale)},
-        {count: 8, type: msgs.get('table.header.deployments', locale)},
-        {count: 2, type: msgs.get('table.header.failedDeployments', locale)},
-      ]
-    }
-  }
 }
 
-const CountCards = ({moduleData: {countCardItems}, handleDrillDownClick, locale}) => {
+const CountCards = ({ moduleData: { countCardItems }, locale }) => {
   return (
     <React.Fragment>
-      {countCardItems.map(({count, type}, idx) => {
-        const onClick = () =>{
-          handleDrillDownClick(type)
-        }
-        const onKeyPress = (e) =>{
-          if ( e.key === 'Enter') {
+      {countCardItems.map(({ count, type }, idx) => {
+        const onClick = () => { }
+        const onKeyPress = (e) => {
+          if (e.key === 'Enter') {
             onClick()
           }
         }
         const cardClasses = classNames({
           'card-count-type': true,
-          hasBorder: idx===0,
+          hasBorder: idx === 0,
         })
         const countClasses = classNames({
           'card-count': true,
           // 'alert': count>0,
         })
         return (
-          <div key={type} className={cardClasses} role={'button'}
-            tabIndex='0' onClick={onClick} onKeyPress={onKeyPress}>
+          <div
+            key={type}
+            className={cardClasses}
+            role="button"
+            tabIndex="0"
+            onClick={onClick}
+            onKeyPress={onKeyPress}
+          >
             <div className={countClasses}>
               {count}
             </div>
-            <div className='card-type'>
+            <div className="card-type">
               <div>
                 {type.toUpperCase()}
               </div>
@@ -91,11 +92,8 @@ const CountCards = ({moduleData: {countCardItems}, handleDrillDownClick, locale}
 }
 
 CountCards.propTypes = {
-  handleDrillDownClick: PropTypes.func,
   locale: PropTypes.string,
   moduleData: PropTypes.object,
 }
 
-CountsCardModule.propTypes = {
-  handleDrillDownClick: PropTypes.func,
-}
+CountsCardModule.propTypes = {}

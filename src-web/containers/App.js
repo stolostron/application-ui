7986692cac7d20e924a0676a146ab59de3650b88
kaapte@us.ibm.com <kaapte@us.ibm.com>
2019-07-17 10:26:19 -0500

    changed context path

--- src-web/containers/App.js
+++ src-web/containers/App.js
@@ -72,7 +72,7 @@ class App extends React.Component {
         {showSecondaryHeader && <SecondaryHeader />}
         <Switch>
           <Route
-            path={`${match.url}/applications`}
+            path={`${match.url}`}
             render={() => <ApplicationHeaderTabs />}
           />
           <Route

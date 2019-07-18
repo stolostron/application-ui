0cef29003313e2928f0ee18708e96f24359f3c46
Richie Piya <richie.piyasirisilp@ibm.com>
2019-07-18 10:21:23 -0400

    added static countCardsModule

--- src-web/components/ApplicationDeployableHighlights/index.js
+++ src-web/components/ApplicationDeployableHighlights/index.js
@@ -8,6 +8,7 @@
  *******************************************************************************/
 
 import React from '../../../node_modules/react';
+import CountsCardModule from '../CountsCardModule';
 import msgs from '../../../nls/platform.properties';
 import { connect } from '../../../node_modules/react-redux';
 import resources from '../../../lib/shared/resources';
@@ -18,7 +19,40 @@ resources(() => {
 
 const mapStateToProps = (state) => {
   const { } = state;
-  return {};
+
+  const countsCardDataGeneralInfo = [
+    {
+      msgKey: 'dashboard.card.deployable.versions',
+      count: 5,
+    },
+    {
+      msgKey: 'dashboard.card.deployable.completed',
+      count: 3,
+    },
+    {
+      msgKey: 'dashboard.card.deployable.failed',
+      count: 1,
+    },
+    {
+      msgKey: 'dashboard.card.deployable.inProgress',
+      count: 1,
+    },
+    {
+      msgKey: 'dashboard.card.deployable.pending',
+      count: 0,
+    },
+  ];
+  const countsCardDataIncidents = [
+    {
+      msgKey: 'dashboard.card.deployable.incidents',
+      count: 1,
+    },
+  ];
+
+  return {
+    countsCardDataGeneralInfo,
+    countsCardDataIncidents,
+  };
 };
 
 class ApplicationDeployableHighlights extends React.Component {
@@ -27,13 +61,29 @@ class ApplicationDeployableHighlights extends React.Component {
 
   render() {
     const { locale } = this.context;
+    const { countsCardDataGeneralInfo, countsCardDataIncidents } = this.props;
 
     return (
-      <div id="ApplicationDeployableHighlights">
-        <div className="deployable-highlights-header">
-          {msgs.get('description.title.deployableHighlights', locale)}
+      <React.Fragment>
+        <div id="ApplicationDeployableHighlights">
+          <div className="deployable-highlights-header">
+            {msgs.get('description.title.deployableHighlights', locale)}
+          </div>
+
+          <div className="deployable-highlights-container">
+            <div className="deployable-highlights-info">
+              <CountsCardModule
+                data={countsCardDataGeneralInfo}
+              />
+            </div>
+            <div className="deployable-highlights-incidents">
+              <CountsCardModule
+                data={countsCardDataIncidents}
+              />
+            </div>
+          </div>
         </div>
-      </div>
+      </React.Fragment>
     );
   }
 }
--- src-web/components/ApplicationDeploymentHighlights/ApplicationDeploymentHighlightsDashboard/index.js
+++ src-web/components/ApplicationDeploymentHighlights/ApplicationDeploymentHighlightsDashboard/index.js
@@ -14,80 +14,80 @@ import { connect } from '../../../../node_modules/react-redux';
 import resources from '../../../../lib/shared/resources';
 
 resources(() => {
-  require('./style.scss');
+    require('./style.scss');
 });
 
 const mapStateToProps = (state) => {
-  const { HCMApplicationList, HCMChannelList, HCMClusterList } = state;
-  const countsCardDataSummary = [
-    {
-      msgKey: 'dashboard.card.deployment.applications',
-      count: getNumItems(HCMApplicationList),
-    },
-    {
-      msgKey: 'dashboard.card.deployment.channels',
-      count: getNumItems(HCMChannelList),
-    },
-    {
-      msgKey: 'dashboard.card.deployment.placementRules',
-      count: 0,
-    },
-    {
-      msgKey: 'dashboard.card.deployment.availabilityZones',
-      count: 0,
-    },
-    {
-      msgKey: 'dashboard.card.deployment.clusters',
-      count: getNumItems(HCMClusterList),
-    },
-  ];
-  const countsCardDataStatus = [
-    {
-      msgKey: 'dashboard.card.deployment.pending',
-      count: 1,
-    },
-    {
-      msgKey: 'dashboard.card.deployment.inProgress',
-      count: 2,
-    },
-    {
-      msgKey: 'dashboard.card.deployment.completed',
-      count: 3,
-    },
-  ];
+    const { HCMApplicationList, HCMChannelList, HCMClusterList } = state;
+    const countsCardDataSummary = [
+        {
+            msgKey: 'dashboard.card.deployment.applications',
+            count: getNumItems(HCMApplicationList),
+        },
+        {
+            msgKey: 'dashboard.card.deployment.channels',
+            count: getNumItems(HCMChannelList),
+        },
+        {
+            msgKey: 'dashboard.card.deployment.placementRules',
+            count: 0,
+        },
+        {
+            msgKey: 'dashboard.card.deployment.availabilityZones',
+            count: 0,
+        },
+        {
+            msgKey: 'dashboard.card.deployment.clusters',
+            count: getNumItems(HCMClusterList),
+        },
+    ];
+    const countsCardDataStatus = [
+        {
+            msgKey: 'dashboard.card.deployment.pending',
+            count: 0,
+        },
+        {
+            msgKey: 'dashboard.card.deployment.inProgress',
+            count: 0,
+        },
+        {
+            msgKey: 'dashboard.card.deployment.completed',
+            count: 0,
+        },
+    ];
 
-  return {
-    countsCardDataSummary,
-    countsCardDataStatus,
-  };
+    return {
+        countsCardDataSummary,
+        countsCardDataStatus,
+    };
 };
 
 class ApplicationDeploymentHighlightsDashboard extends React.Component {
-  componentDidMount() {}
-  componentWillUnmount() {}
+    componentDidMount() { }
+    componentWillUnmount() { }
 
-  render() {
-    const { countsCardDataSummary, countsCardDataStatus } = this.props;
+    render() {
+        const { countsCardDataSummary, countsCardDataStatus } = this.props;
 
-    return (
-      <React.Fragment>
-        <div id="ApplicationDeploymentsDashboard">
-          <div className="deployment-summary">
-            <CountsCardModule
-              data={countsCardDataSummary}
-              title="dashboard.card.deployment.summary.title"
-            />
-          </div>
-          <div className="deployment-status">
-            <CountsCardModule
-              data={countsCardDataStatus}
-              title="dashboard.card.deployment.status.title"
-            />
-          </div>
-        </div>
-      </React.Fragment>
-    );
-  }
+        return (
+            <React.Fragment>
+                <div id="ApplicationDeploymentsDashboard">
+                    <div className="deployment-summary">
+                        <CountsCardModule
+                            data={countsCardDataSummary}
+                            title="dashboard.card.deployment.summary.title"
+                        />
+                    </div>
+                    <div className="deployment-status">
+                        <CountsCardModule
+                            data={countsCardDataStatus}
+                            title="dashboard.card.deployment.status.title"
+                        />
+                    </div>
+                </div>
+            </React.Fragment>
+        );
+    }
 }
 
 export default connect(mapStateToProps)(ApplicationDeploymentHighlightsDashboard);

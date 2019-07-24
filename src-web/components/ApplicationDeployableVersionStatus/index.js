2efe87c6c10411a44fee8e83c4ea37758a2b4f91
Harry Li <harryli@ca.ibm.com>
2019-07-24 14:20:38 -0400

    first iteration of version status table

--- src-web/components/ApplicationDeployableVersionStatus/index.js
+++ src-web/components/ApplicationDeployableVersionStatus/index.js
@@ -9,6 +9,7 @@
 
 import React from '../../../node_modules/react';
 import msgs from '../../../nls/platform.properties';
+import { Tile, Icon } from 'carbon-components-react'
 import { withLocale } from '../../providers/LocaleProvider';
 import { connect } from '../../../node_modules/react-redux';
 import resources from '../../../lib/shared/resources';
@@ -17,16 +18,77 @@ resources(() => {
   require('./style.scss');
 });
 
+
+const deployableColumns = (channels) => {
+
+
+
+
+
+
+  return (
+    <div class="versionStatusGridContainer">
+      <div class="horizontalScrollRow">
+        {channels.map((channel) => {
+          return (
+            <div class="versionStatusColumn">
+
+              <Tile>
+                <div class="yaml-edit">YAML <Icon name="icon--edit" fill="#6089bf"
+                  className="channelEditIcon" /></div>
+                <div class="environment"> {channel.name} </div>
+                <div class="gate-conditions">Gate conditions to arrive to this channel</div>
+              </Tile>
+
+            </div>
+          );
+
+        }
+        )
+        }
+      </div>
+
+
+
+      <div class="horizontalScrollRow">
+        {channels.map((channel) => {
+          return (
+            <div class="versionStatusColumn">
+
+              <Tile>
+                <span class="statusTag"> {channel.status}</span>
+
+
+                <span class="lastUpdateTime">{channel.lastUpdateTime}</span>
+              </Tile>
+            </div>
+          );
+
+        }
+        )
+        }
+      </div></div>);
+
+
+};
+
+
 const ApplicationDeployableVersionStatus = withLocale(({ deployableDetails, channels, subscriptions, locale }) => {
   return (
     <div id="ApplicationDeployableVersionStatus" >
-      <p>Channels: {JSON.stringify(channels)}</p>
-      <p>Channels: {JSON.stringify(subscriptions)}</p>
 
-      <h1>name: {deployableDetails.deployables.metadata.name} </h1>
-      <h1>Version: {JSON.stringify(subscriptions.version)}</h1>
+
       <div className="deployable-versionStatus-header">
         {msgs.get('description.title.deployableVersionStatus', locale)}
+      </div>
+
+      <div className="deployable-grouping">
+        <div className="deployableLeftColumn">
+          <Tile></Tile>
+          <Tile>{deployableDetails.deployables.metadata.name} {subscriptions.version}</Tile>
+        </div>
+
+        {deployableColumns(channels)}
       </div>
     </div >
   );

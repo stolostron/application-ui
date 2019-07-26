14525a75eabe303ceb54147621b7bab2da490bc2
Harry Li <harryli@ca.ibm.com>
2019-07-26 17:24:20 -0400

    moving get logic to utils.js

--- src-web/containers/ApplicationDeployableDetails/index.js
+++ src-web/containers/ApplicationDeployableDetails/index.js
@@ -13,7 +13,13 @@ import resources from '../../../lib/shared/resources'
 import {
   updateSecondaryHeader /* , fetchResource */
 } from '../../actions/common'
-import { getBreadCrumbs } from './utils'
+import {
+  getBreadCrumbs,
+  getDeployableDetails,
+  getSubscriptions,
+  getChannels,
+  sampleData
+} from './utils'
 import ApplicationDeployableHighlights from '../../components/ApplicationDeployableHighlights'
 import ApplicationDeployableSubscription from '../../components/ApplicationDeployableSubscription'
 import ApplicationDeployableVersionStatus from '../../components/ApplicationDeployableVersionStatus'
@@ -30,119 +36,13 @@ const mapDispatchToProps = dispatch => {
   }
 }
 
-const tempData = {
-  deployables: {
-    metadata: {
-      name: 'kirti1-guestbook-app',
-      selfLink:
-        '/apis/mcm.ibm.com/v1alpha1/namespaces/ibmcom/deployables/kirti1-guestbook-app',
-      __typename: 'Metadata',
-    },
-    status: {
-      cluster1: 'failed',
-      cluster2: 'success',
-    },
-    channels: [
-      {
-        name: 'dev',
-        template: {
-          kind: 'CustomResourceDefinition',
-          scope: 'Namespaced',
-        },
-        lastUpdateTime: '2019-07-05T09:50:56Z',
-        status: 'failed',
-      },
-      {
-        name: 'devkirti',
-        template: {
-          kind: 'CustomResourceDefinition',
-          scope: 'Namespaced',
-        },
-        lastUpdateTime: '2019-07-05T09:50:56Z',
-        status: 'success',
-      },
-      {
-        name: 'prod',
-        template: {
-          kind: 'CustomResourceDefinition',
-          scope: 'Namespaced',
-        },
-        lastUpdateTime: '2019-07-05T09:50:56Z',
-        status: 'success',
-      },
-    ],
-    subscription: {
-      metadata: {
-        name: 'mydevsub',
-        namespace: 'myspace',
-        labels: [
-          ['Region', 'NorthAmerica'],
-          ['DataCenter', 'Austin'],
-          ['ClusterType', 'AWS'],
-          ['controller-tools.k8s.io', '1.0'],
-        ],
-      },
-      spec: {
-        channel: 'ch - dev / dev',
-        package: 'nginx',
-        packageFilter: {
-          version: '1. x',
-        },
-        version: 'v1alpha1',
-      },
-      placement: {
-        clusters: {
-          '-name': 'mydevcluster1',
-        },
-      },
-    },
-    __typename: 'Deployable',
-  },
-};
+const mapStateToProps = () => {
+  const deployableDetails = getDeployableDetails(sampleData)
+  const subscriptions = getSubscriptions(sampleData)
+  const channels = getChannels(sampleData)
 
-export const getDeployableDetails = (data) => {
-  if (data) {
-    // perhaps restructure the object that is returned
-    return data;
-  }
-  return [];
-};
-
-export const getSubscriptions = (data) => {
-  if (data && data.deployables && data.deployables.subscription) {
-    // perhaps restructure the object that is returned
-
-    return {
-      name: data.deployables.subscription.metadata.name || '',
-      namespace: data.deployables.subscription.metadata.namespace || '',
-      labels: data.deployables.subscription.metadata.labels || '',
-      channel: data.deployables.subscription.spec.channel || '',
-      package: data.deployables.subscription.spec.package || '',
-      version: data.deployables.subscription.spec.version || '',
-      clusters: data.deployables.subscription.placement.clusters || '',
-    };
-  }
-  return [];
-};
-
-export const getChannels = (data) => {
-
-  if (data && data.deployables && data.deployables.channels) {
-    return data.deployables.channels;
-  }
-  return [];
-
+  return { deployableDetails, subscriptions, channels }
 }
-
-const mapStateToProps = (state) => {
-  const { } = state;
-
-  const deployableDetails = getDeployableDetails(tempData);
-  const subscriptions = getSubscriptions(tempData);
-  const channels = getChannels(tempData);
-
-  return { deployableDetails, subscriptions, channels };
-};
 
 class ApplicationDeployableDetails extends React.Component {
   componentWillMount() {
@@ -155,9 +55,9 @@ class ApplicationDeployableDetails extends React.Component {
     updateSecondaryHeaderInfo(deployableParams.name || '', breadCrumbs)
   }
 
-  componentDidMount() { }
+  componentDidMount() {}
 
-  componentWillUnmount() { }
+  componentWillUnmount() {}
 
   render() {
     return (

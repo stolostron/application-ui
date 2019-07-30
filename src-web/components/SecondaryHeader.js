feb505e30f437493332e1d9051c523b903f398e9
Lin Song <lin.song@ibm.com>
2019-07-30 16:28:53 -0500

    fix blank space in header

--- src-web/components/SecondaryHeader.js
+++ src-web/components/SecondaryHeader.js
@@ -34,34 +34,61 @@ export class SecondaryHeader extends React.Component {
   render() {
     const { tabs, title, breadcrumbItems } = this.props
     const { locale } = this.context
-    if ((tabs && tabs.length > 0) || (breadcrumbItems && breadcrumbItems.length > 0)) {
+    if (tabs && tabs.length > 0) {
       return (
-        <div className='secondary-header-wrapper' role='region' aria-label={title}>
-          <div className='secondary-header'>
-            {tabs && tabs.length > 0 ? (
-              <DetailPageHeader hasTabs={true} title={decodeURIComponent(title)} aria-label={`${title} ${msgs.get('secondaryHeader', locale)}`}>
-                <Breadcrumb>
-                  {breadcrumbItems && this.renderBreadCrumb()}
-                </Breadcrumb>
-                <Tabs selected={this.getSelectedTab() || 0} aria-label={`${title} ${msgs.get('tabs.label', locale)}`}>
-                  {this.renderTabs()}
-                </Tabs>
-              </DetailPageHeader>
-            ) : (
-              <DetailPageHeader hasTabs={true} title={decodeURIComponent(title)} aria-label={`${title} ${msgs.get('secondaryHeader', locale)}`}>
-                <Breadcrumb>
-                  {this.renderBreadCrumb()}
-                </Breadcrumb>
-              </DetailPageHeader>
-            )}
+        <div
+          className="secondary-header-wrapper"
+          role="region"
+          aria-label={title}
+        >
+          <div className="secondary-header">
+            <DetailPageHeader
+              hasTabs={true}
+              title={decodeURIComponent(title)}
+              aria-label={`${title} ${msgs.get('secondaryHeader', locale)}`}
+            >
+              <Breadcrumb>
+                {breadcrumbItems && this.renderBreadCrumb()}
+              </Breadcrumb>
+              <Tabs
+                selected={this.getSelectedTab() || 0}
+                aria-label={`${title} ${msgs.get('tabs.label', locale)}`}
+              >
+                {this.renderTabs()}
+              </Tabs>
+            </DetailPageHeader>
+          </div>
+        </div>
+      )
+    } else if (breadcrumbItems && breadcrumbItems.length > 0) {
+      return (
+        <div
+          className="secondary-header-wrapper-no-tabs"
+          role="region"
+          aria-label={title}
+        >
+          <div className="secondary-header">
+            <DetailPageHeader
+              hasTabs={false}
+              title={decodeURIComponent(title)}
+              aria-label={`${title} ${msgs.get('secondaryHeader', locale)}`}
+            >
+              <Breadcrumb>{this.renderBreadCrumb()}</Breadcrumb>
+            </DetailPageHeader>
           </div>
         </div>
       )
     } else {
       return (
-        <div className='secondary-header-wrapper-min' role='region' aria-label={`${title} ${msgs.get('secondaryHeader', locale)}`}>
-          <div className='secondary-header simple-header'>
-            <h1 className='bx--detail-page-header-title'>{decodeURIComponent(title)}</h1>
+        <div
+          className="secondary-header-wrapper-min"
+          role="region"
+          aria-label={`${title} ${msgs.get('secondaryHeader', locale)}`}
+        >
+          <div className="secondary-header simple-header">
+            <h1 className="bx--detail-page-header-title">
+              {decodeURIComponent(title)}
+            </h1>
           </div>
         </div>
       )
@@ -70,34 +97,57 @@ export class SecondaryHeader extends React.Component {
 
   renderBreadCrumb() {
     const { breadcrumbItems } = this.props
-    return breadcrumbItems && breadcrumbItems.map((breadcrumb, index) => {
-      const key = `${breadcrumb}-${index}`
-      return (
-        <div key={key} className='bx--breadcrumb-item' title={decodeURIComponent(breadcrumb.label)}>
-          <Link to={breadcrumb.url} className='bx--link'>{decodeURIComponent(breadcrumb.label)}</Link>
-        </div>
-      )
-    })
+    return (
+      breadcrumbItems &&
+      breadcrumbItems.map((breadcrumb, index) => {
+        const key = `${breadcrumb}-${index}`
+        return (
+          <div
+            key={key}
+            className="bx--breadcrumb-item"
+            title={decodeURIComponent(breadcrumb.label)}
+          >
+            <Link to={breadcrumb.url} className="bx--link">
+              {decodeURIComponent(breadcrumb.label)}
+            </Link>
+          </div>
+        )
+      })
+    )
   }
 
   renderTabs() {
     const { tabs } = this.props,
           { locale } = this.context
     return tabs.map(tab => {
-      return <Tab label={msgs.get(tab.label, locale)} key={tab.id} id={tab.id} href={tab.url} onClick={tab.handleClick ? tab.handleClick : this.clickTab.bind(this, tab.url)} />
+      return (
+        <Tab
+          label={msgs.get(tab.label, locale)}
+          key={tab.id}
+          id={tab.id}
+          href={tab.url}
+          onClick={
+            tab.handleClick
+              ? tab.handleClick
+              : this.clickTab.bind(this, tab.url)
+          }
+        />
+      )
     })
   }
 
   getSelectedTab() {
     const { tabs, location } = this.props
-    const selectedTab = tabs.map((tab, index) => {
-      tab.index = index
-      return tab
-    }).filter((tab, index) => {
-      if (index !== 0) {
-        return location.pathname.startsWith(tab.url)
-      }
-    })
+    const selectedTab = tabs
+      .map((tab, index) => {
+        tab.index = index
+        return tab
+      })
+      .filter((tab, index) => {
+        if (index !== 0) {
+          return location.pathname.startsWith(tab.url)
+        }
+      })
     return selectedTab[0] && selectedTab[0].index
   }
 
@@ -110,13 +160,13 @@ SecondaryHeader.contextTypes = {
   locale: PropTypes.string
 }
 
-const mapStateToProps = (state) => {
+const mapStateToProps = state => {
   return {
     title: state.secondaryHeader.title,
     tabs: state.secondaryHeader.tabs,
     breadcrumbItems: state.secondaryHeader.breadcrumbItems,
     links: state.secondaryHeader.links,
-    role: state.role && state.role.role,
+    role: state.role && state.role.role
   }
 }
 

d89938e04e8be766259acbc7f5970a2f5ced1885
Richie Piya <richie.piyasirisilp@ibm.com>
2019-08-11 21:39:45 -0400

    changed to class

--- src-web/components/ApplicationLogs/index.js
+++ src-web/components/ApplicationLogs/index.js
@@ -11,53 +11,56 @@ import msgs from '../../../nls/platform.properties'
 import { Icon } from 'carbon-components-react'
 import ScrollBox from '../modals/ScrollBox'
 import { DropdownV2 } from 'carbon-components-react'
-import { withLocale } from '../../providers/LocaleProvider'
 import resources from '../../../lib/shared/resources'
 
 resources(() => {
   require('./style.scss')
 })
 
-const ApplicationLogs = withLocale(({ locale }) => {
-  const podItems = ["pod1", "pod2", "pod3"]
-  const containerItems = ["container1", "container2", "container3"]
-  const logsContent = "Testing logs..."
+class ApplicationLogs extends React.Component {
 
-  return (
-    <React.Fragment>
-      <div id="ApplicationLogs">
-        <div className="dropdown-container">
-          <div className="dropdown-pods-list">
-            <DropdownV2
-              ariaLabel={msgs.get("dropdown.pod.label", locale)}
-              light
-              label="Pods"
-              // onChange={this.handleContainerChange.bind(this)}
-              items={podItems} />
+  render() {
+    const { locale } = this.context
+    const podItems = ["pod1", "pod2", "pod3"]
+    const containerItems = ["container1", "container2", "container3"]
+    const logsContent = "Testing logs..."
+
+    return (
+      <React.Fragment>
+        <div id="ApplicationLogs">
+          <div className="dropdown-container">
+            <div className="dropdown-pods-list">
+              <DropdownV2
+                ariaLabel={msgs.get("dropdown.pod.label", locale)}
+                light
+                label="Pods"
+                // onChange={this.handleContainerChange.bind(this)}
+                items={podItems} />
+            </div>
+            <span className="dropdown-divider">{msgs.get("tabs.logs.dropdown.divider")}</span>
+            <div className="dropdown-containers-list">
+              <DropdownV2
+                ariaLabel={msgs.get("dropdown.pod.label", locale)}
+                light
+                label="Containers"
+                // onChange={this.handleContainerChange.bind(this)}
+                items={containerItems} />
+            </div>
+            <div className="view-external-container">
+              <p className="viewExternalIconTitle">{msgs.get("tabs.logs.viewExternal")}</p>
+              <Icon
+                name="icon--launch"
+                fill="#6089bf"
+                description=""
+                className="viewExternalIcon"
+              />
+            </div>
           </div>
-          <span className="dropdown-divider">{msgs.get("tabs.logs.dropdown.divider")}</span>
-          <div className="dropdown-containers-list">
-            <DropdownV2
-              ariaLabel={msgs.get("dropdown.pod.label", locale)}
-              light
-              label="Containers"
-              // onChange={this.handleContainerChange.bind(this)}
-              items={containerItems} />
-          </div>
-          <div className="view-external-container">
-            <p className="viewExternalIconTitle">{msgs.get("tabs.logs.viewExternal")}</p>
-            <Icon
-              name="icon--launch"
-              fill="#6089bf"
-              description=""
-              className="viewExternalIcon"
-            />
-          </div>
+          <ScrollBox className="logs-container__content" content={logsContent} />
         </div>
-        <ScrollBox className="logs-container__content" content={logsContent} />
-      </div>
-    </React.Fragment>
-  )
-})
+      </React.Fragment>
+    )
+  }
+}
 
-export default withLocale(ApplicationLogs)
+export default ApplicationLogs

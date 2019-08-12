01503de1d2c017e26368e3cfc58915cb8344b618
Lin Song <lin.song@ibm.com>
2019-08-12 16:54:44 -0500

    display application details using new API

--- src-web/reducers/data-mappers/mapApplicationsSingle.js
+++ src-web/reducers/data-mappers/mapApplicationsSingle.js
@@ -11,6 +11,50 @@
 export const mapSingleApplication = application => {
   if (application && application.items && application.related) {
     const items = application.items[0]
+    let deploymentsList = []
+    let deployablesList = []
+    let placementRulesList = []
+    let subscriptionsList = []
+    const deployments = application.related.filter(
+      elem =>
+        ![
+          'deployable',
+          'channel',
+          'cluster',
+          'subscription',
+          'placementbinding',
+          'placementrule'
+        ].includes(elem.kind)
+    )
+    const deployables = application.related.filter(
+      elem => elem.kind === 'deployable'
+    )
+    const placementRules = application.related.filter(
+      elem => elem.kind === 'placementrule'
+    )
+    const subscriptions = application.related.filter(
+      elem => elem.kind === 'subscription'
+    )
+    deployments.map(elem => {
+      if (elem.items instanceof Array) {
+        deploymentsList = deploymentsList.concat(elem.items)
+      }
+    })
+    deployables.map(elem => {
+      if (elem.items instanceof Array) {
+        deployablesList = deployablesList.concat(elem.items)
+      }
+    })
+    placementRules.map(elem => {
+      if (elem.items instanceof Array) {
+        placementRulesList = placementRulesList.concat(elem.items)
+      }
+    })
+    subscriptions.map(elem => {
+      if (elem.items instanceof Array) {
+        subscriptionsList = subscriptionsList.concat(elem.items)
+      }
+    })
     return [
       {
         name: items.name || '',
@@ -25,7 +69,11 @@ export const mapSingleApplication = application => {
         label: items.label || '',
         _hubClusterResource: items._hubClusterResource || '',
         _rbac: items._rbac || '',
-        related: application.related || []
+        related: application.related || [],
+        deployments: deploymentsList,
+        deployables: deployablesList,
+        placementRules: placementRulesList,
+        subscriptions: subscriptionsList
       }
     ]
   }

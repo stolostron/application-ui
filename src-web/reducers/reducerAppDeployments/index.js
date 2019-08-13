f494287e80eaf1114a897bc9bbe18e13eed57dbf
Josh D. King <jdking@us.ibm.com>
2019-08-13 10:08:23 -0500

    initial commit set up for api call

--- src-web/reducers/reducerAppDeployments/index.js
+++ src-web/reducers/reducerAppDeployments/index.js
@@ -9,10 +9,14 @@
 
 // @flow
 import { createAction } from '../../shared/utils/state'
+import { SEARCH_QUERY_RELATED } from '../../apollo-client/queries/SearchQueries'
+import { getQueryStringForResource } from '../../actions/common'
 import R from 'ramda'
 
 const OPEN_DISPLAY_DEPLOYABLE_MODAL = 'OPEN_DISPLAY_DEPLOYABLE_MODAL'
 const UPDATE_APP_DROPDOWN_LIST = 'UPDATE_APP_DROPDOWN_LIST'
+const SET_BULK_DEPLOYABLE_LIST = 'SET_BULK_DEPLOYABLE_LIST'
+const SET_BULK_DEPLOYABLE_ERROR = 'SET_BULK_DEPLOYABLE_ERROR'
 const SET_DEPLOYABLE_MODAL_HEADERS = 'SET_DEPLOYABLE_MODAL_HEADERS'
 const SET_DEPLOYABLE_SUBSCRIPTION_INFO = 'SET_DEPLOYABLE_SUBSCRIPTION_INFO'
 const SET_DEPLOYMENT_SEARCH = 'SET_DEPLOYMENT_SEARCH'
@@ -30,6 +34,8 @@ export const initialStateDeployments = {
   deployableModalSubscriptionInfo: {},
   deploymentPipelineSearch: '',
   currentChannelInfo: {},
+  bulkDeployableList: [],
+  bulkDeployableError: '',
   openEditChannelModal: false,
   loading: false
 }
@@ -48,6 +54,12 @@ export const AppDeployments = (state = initialStateDeployments, action) => {
   }
   case SET_DEPLOYABLE_SUBSCRIPTION_INFO: {
     return { ...state, deployableModalSubscriptionInfo: action.payload }
+  }
+  case SET_BULK_DEPLOYABLE_LIST: {
+    return { ...state, bulkDeployableList: action.payload }
+  }
+  case SET_BULK_DEPLOYABLE_ERROR: {
+    return { ...state, bulkDeployableError: action.payload }
   }
   case UPDATE_APP_DROPDOWN_LIST: {
     const containsApp = state.appDropDownList.includes(action.payload)
@@ -91,6 +103,8 @@ export const AppDeployments = (state = initialStateDeployments, action) => {
 export default AppDeployments
 
 export const setDeploymentSearch = createAction(SET_DEPLOYMENT_SEARCH)
+export const setBulkDeployableList = createAction(SET_BULK_DEPLOYABLE_LIST)
+export const setBulkDeployableError = createAction(SET_BULK_DEPLOYABLE_ERROR)
 export const setDeployableModalHeaderInfo = createAction(
   SET_DEPLOYABLE_MODAL_HEADERS
 )
@@ -134,6 +148,33 @@ export const fetchChannelResource = (
       .catch(err => {
         dispatch(setLoading(false))
         dispatch(setCurrentChannelInfo(err))
+      })
+  }
+}
+
+export const fetchBulkDeployableList = (apolloClient, applicationList) => {
+  const itemRes =
+    applicationList &&
+    applicationList.data &&
+    applicationList.data.searchResult[0] &&
+    applicationList.data.searchResult[0].items
+  const combinedQuery = []
+  itemRes.map(item => {
+    combinedQuery.push(
+      getQueryStringForResource(`kind:deployable name:${item.name} namespace:${item.namespace}`)
+    )
+  })
+  return dispatch => {
+    return apolloClient
+      .search(SEARCH_QUERY_RELATED, { input: combinedQuery })
+      .then(response => {
+        if (response.errors) {
+          return dispatch(setBulkDeployableError(response.errors))
+        }
+        dispatch(setBulkDeployableList(response))
+      })
+      .catch(err => {
+        dispatch(setBulkDeployableError(error))
       })
   }
 }

bfa88559314cbe0d789cfeb1a8c6eef57ee71b0c
kaapte@us.ibm.com <kaapte@us.ibm.com>
2019-09-06 11:56:49 -0500

    text missing

--- src-web/components/ApplicationDeploymentHighlights/ApplicationDeploymentHighlightsDashboard/index.js
+++ src-web/components/ApplicationDeploymentHighlights/ApplicationDeploymentHighlightsDashboard/index.js
@@ -89,7 +89,7 @@ const countsCardDataSummary = (
           ? 'dashboard.card.deployment.clusters'
           : 'dashboard.card.deployment.cluster',
       count: clusters,
-      textKey: 'ashboard.card.deployment.clustersInfo',
+      textKey: 'dashboard.card.deployment.clustersInfo',
       targetLink
     }
   ]

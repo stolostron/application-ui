2ca7a2fd1324a4d13a0ca5d491da5dc01c2ad0a2
Richie Piya <richie.piyasirisilp@ibm.com>
2019-10-17 12:50:08 -0400

    updated import method

--- src-web/shared/yamlSamples/index.js
+++ src-web/shared/yamlSamples/index.js
@@ -7,69 +7,26 @@
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  *******************************************************************************/
 
+import YAML from 'yaml'
 import msgs from '../../../nls/platform.properties'
 import channelNamespaceSample from 'js-yaml-loader!./channelNamespaceSample.yml'
 
-// for (var prop in yamlObj) {
-//   if (Object.prototype.hasOwnProperty.call(yamlObj, prop)) {
-//     console.log(yamlObj[prop])
-//   }
-// }
-// Object.keys(channelNamespaceSample)
-// {YAML.stringify(samples.createChannelSample).split("null").join("")}
-
 export const getChannelNamespaceSample = () => {
-  const sample =
-    'apiVersion: ' +
-    channelNamespaceSample.apiVersion +
-    '\t# ' +
-    msgs.get('description.createChannel.apiVersion') +
-    '\n' +
-    'kind: ' +
-    channelNamespaceSample.kind +
-    '\t\t\t\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.kind') +
-    '\n' +
-    'metadata: ' +
-    '\n' +
-    '\tname: ' +
-    channelNamespaceSample.metadata.name +
-    '\t\t\t\t\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.metadata.name') +
-    '\n' +
-    '\tnamespace: ' +
-    channelNamespaceSample.metadata.namespace +
-    '\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.metadata.namespace') +
-    '\n' +
-    'spec: ' +
-    '\n' +
-    '\tsourceNamespaces: ' +
-    '\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.spec.sourceNamespaces') +
-    '\n' +
-    '\t- default' +
-    '\n' +
-    '\ttype: ' +
-    channelNamespaceSample.spec.type +
-    '\t\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.spec.type') +
-    '\n' +
-    '\tpathname: ' +
-    channelNamespaceSample.spec.pathname +
-    '\t\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.spec.pathname') +
-    '\n' +
-    '\tgates: ' +
-    '\t\t\t\t\t\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.spec.gates') +
-    '\n' +
-    '\t\tannotations: ' +
-    '\t\t\t\t\t\t\t\t\t# ' +
-    msgs.get('description.createChannel.spec.gates.annotations') +
-    '\n' +
-    '\t\t\tdev-ready: ' +
-    channelNamespaceSample.spec.gates.annotations['dev-ready']
+  const mapObj = {
+    "createChannel-apiVersion": "# " + msgs.get('description.createChannel.apiVersion'),
+    "createChannel-kind": "# " + msgs.get('description.createChannel.kind'),
+    "createChannel-metadata-name": "# " + msgs.get('description.createChannel.metadata.name'),
+    "createChannel-metadata-namespace": "# " + msgs.get('description.createChannel.metadata.namespace'),
+    "createChannel-spec-gates-annotations": "# " + msgs.get('description.createChannel.spec.gates.annotations'),
+    "createChannel-spec-sourceNamespaces": "# " + msgs.get('description.createChannel.spec.sourceNamespaces'),
+    "createChannel-spec-type": "# " + msgs.get('description.createChannel.spec.type'),
+    "null": "",
+    "_": " "
+  };
+
+  var sample = YAML.stringify(channelNamespaceSample).replace(/createChannel-apiVersion|createChannel-kind|createChannel-metadata-name|createChannel-metadata-namespace|createChannel-spec-gates-annotations|createChannel-spec-sourceNamespaces|createChannel-spec-type|null|_/gi, function (matched) {
+    return mapObj[matched]
+  })
 
   return sample
 }

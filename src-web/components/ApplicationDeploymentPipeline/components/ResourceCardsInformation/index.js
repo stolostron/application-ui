a88233339587ecb73e7a5b1c7e0be2690adc7483
Richie Piya <richie.piyasirisilp@ibm.com>
2019-10-24 16:07:19 -0400

    adding data to info cards

--- src-web/components/ApplicationDeploymentPipeline/components/ResourceCardsInformation/index.js
+++ src-web/components/ApplicationDeploymentPipeline/components/ResourceCardsInformation/index.js
@@ -9,14 +9,25 @@
 
 import R from 'ramda'
 import React from 'react'
-
 import { connect } from 'react-redux'
 import resources from '../../../../../lib/shared/resources'
 import { RESOURCE_TYPES } from '../../../../../lib/shared/constants'
 import { fetchResources } from '../../../../actions/common'
 import msgs from '../../../../../nls/platform.properties'
-import { getSingleApplicationObject } from '../../../ApplicationDeploymentHighlights/ApplicationDeploymentHighlightsDashboard/utils'
-import { pullOutKindPerApplication } from '../../../ApplicationDeploymentPipeline/utils'
+import {
+  getNumClusters,
+  getApplicationName,
+  getSingleApplicationObject,
+  getChannelsCountFromSubscriptions
+} from './utils'
+import {
+  getSearchLinkForOneApplication,
+  getSearchLinkForAllApplications,
+  getSearchLinkForAllChannels,
+  getSearchLinkForAllSubscriptions
+} from '../../../common/ResourceOverview/utils'
+import { pullOutKindPerApplication } from '../../utils'
+import { getNumItems } from '../../../../../lib/client/resource-helper'
 
 /* eslint-disable react/prop-types */
 
@@ -47,6 +58,96 @@ const mapStateToProps = state => {
   }
 }
 
+const getResourceCardsData = (
+  HCMApplicationList,
+  HCMChannelList,
+  HCMSubscriptionList,
+  isSingleApplicationView,
+  targetLink,
+  targetLinkForSubscriptions,
+  targetLinkForChannels,
+  locale
+) => {
+  const applications = getNumItems(HCMApplicationList)
+  const clusters = getNumClusters(HCMApplicationList, HCMSubscriptionList)
+  let channels = getNumItems(HCMChannelList)
+
+  //count only hub subscriptions
+  const isHubSubscr = item => !item._hostingSubscription
+  let subscriptions = getNumItems(HCMSubscriptionList, isHubSubscr)
+  if (isSingleApplicationView) {
+    const subscriptionsArray = pullOutKindPerApplication(
+      getSingleApplicationObject(HCMApplicationList),
+      'subscription'
+    )
+    subscriptions =
+      subscriptionsArray &&
+      subscriptionsArray.length > 0 &&
+      subscriptionsArray[0] &&
+      subscriptionsArray[0].items &&
+      subscriptionsArray[0].items instanceof Array
+        ? subscriptionsArray[0].items.length
+        : 0
+    channels = getChannelsCountFromSubscriptions(subscriptionsArray)
+  }
+
+  const result = [
+    {
+      msgKey:
+        subscriptions > 1
+          ? msgs.get('dashboard.card.deployment.subscriptions', locale)
+          : msgs.get('dashboard.card.deployment.subscription', locale),
+      count: subscriptions,
+      targetLink: targetLinkForSubscriptions,
+      textKey: msgs.get('dashboard.card.deployment.subscriptions.text', locale),
+      subtextKey: [
+        msgs.get('dashboard.card.deployment.failed', locale),
+        msgs.get('dashboard.card.deployment.noStatus', locale)
+      ]
+    },
+    {
+      msgKey:
+        clusters > 1
+          ? msgs.get('dashboard.card.deployment.clusters', locale)
+          : msgs.get('dashboard.card.deployment.cluster', locale),
+      count: clusters,
+      targetLink,
+      textKey: applications
+        .toString()
+        .concat(
+          ' ',
+          applications > 1
+            ? msgs.get('dashboard.card.deployment.totalSubscriptions', locale)
+            : msgs.get('dashboard.card.deployment.totalSubscription', locale)
+        ),
+      subtextKey: [
+        msgs.get('dashboard.card.deployment.failed', locale),
+        msgs.get('dashboard.card.deployment.noStatus', locale)
+      ]
+    },
+    {
+      msgKey:
+        channels > 1
+          ? msgs.get('dashboard.card.deployment.channels', locale)
+          : msgs.get('dashboard.card.deployment.channel', locale),
+      count: channels,
+      targetLink: targetLinkForChannels,
+      textKey: msgs.get('dashboard.card.deployment.total', locale)
+    },
+    {
+      msgKey:
+        applications > 1
+          ? msgs.get('dashboard.card.deployment.placementRules', locale)
+          : msgs.get('dashboard.card.deployment.placementRule', locale),
+      count: 123,
+      targetLink,
+      textKey: msgs.get('dashboard.card.deployment.total', locale)
+    }
+  ]
+
+  return result
+}
+
 class ResourceCardsInformation extends React.Component {
   componentWillMount() {}
   componentDidMount() {}
@@ -63,76 +164,62 @@ class ResourceCardsInformation extends React.Component {
     const { locale } = this.context
     const singleAppStyle = isSingleApplicationView ? ' single-app' : ''
 
-    // return (
-    //   <div id="DeploymentHighlights">
-    //     <ApplicationDeploymentHighlightsDashboard
-    //       HCMApplicationList={HCMApplicationList}
-    //       HCMChannelList={HCMChannelList}
-    //       HCMSubscriptionList={HCMSubscriptionList}
-    //       isSingleApplicationView={isSingleApplicationView}
-    //     />
-    //   </div>
-    // )
+    const applicationName = getApplicationName(HCMApplicationList)
+    const targetLink = isSingleApplicationView
+      ? getSearchLinkForOneApplication({
+        name: encodeURIComponent(applicationName)
+      })
+      : getSearchLinkForAllApplications()
+
+    const targetLinkForSubscriptions = isSingleApplicationView
+      ? getSearchLinkForOneApplication({
+        name: encodeURIComponent(applicationName)
+      })
+      : getSearchLinkForAllSubscriptions()
+
+    const targetLinkForChannels = isSingleApplicationView
+      ? getSearchLinkForOneApplication({
+        name: encodeURIComponent(applicationName)
+      })
+      : getSearchLinkForAllChannels()
+
+    const resourceCardsData = getResourceCardsData(
+      HCMApplicationList,
+      HCMChannelList,
+      HCMSubscriptionList,
+      isSingleApplicationView,
+      targetLink,
+      targetLinkForSubscriptions,
+      targetLinkForChannels,
+      locale
+    )
+    const onClick = i => {
+      window.open(resourceCardsData[i].targetLink, '_blank')
+    }
+    const onKeyPress = (e, i) => {
+      if (e.key === 'Enter') {
+        onClick(i)
+      }
+    }
 
     return (
       <div className={'resource-cards-info' + singleAppStyle}>
-        <div
-          key="1"
-          className="first-item"
-          role="button"
-          tabIndex="0"
-          // onClick={onClick}
-          // onKeyPress={onKeyPress}
-        >
-          <div className="card-count">20</div>
-          <div className="card-type">Subscriptions</div>
-          <div className="card-text">Hub</div>
-          <div className="row-divider" />
-          <div className="card-text">Failed</div>
-          <div className="card-text">No Status</div>
-        </div>
-        <div className="column-divider" />
-        <div
-          key="2"
-          className="second-item"
-          role="button"
-          tabIndex="0"
-          // onClick={onClick}
-          // onKeyPress={onKeyPress}
-        >
-          <div className="card-count">30</div>
-          <div className="card-type">Managed Clusters</div>
-          <div className="card-text">Total Subscriptions</div>
-          <div className="row-divider" />
-          <div className="card-text">Failed</div>
-          <div className="card-text">No Status</div>
-        </div>
-        <div className="column-divider" />
-        <div
-          key="3"
-          className="third-item"
-          role="button"
-          tabIndex="0"
-          // onClick={onClick}
-          // onKeyPress={onKeyPress}
-        >
-          <div className="card-count">5</div>
-          <div className="card-type">Channels</div>
-          <div className="card-text">Total</div>
-        </div>
-        <div className="column-divider" />
-        <div
-          key="4"
-          className="fourth-item"
-          role="button"
-          tabIndex="0"
-          // onClick={onClick}
-          // onKeyPress={onKeyPress}
-        >
-          <div className="card-count">5</div>
-          <div className="card-type">Placement Rules</div>
-          <div className="card-text">Total</div>
-        </div>
+        {Object.keys(resourceCardsData).map(key => {
+          return (
+            <div
+              key={resourceCardsData[key]}
+              className="resource-card"
+              role="button"
+              tabIndex="0"
+              onClick={onClick(key)}
+              onKeyPress={onKeyPress(key)}
+            >
+              <div className="card-count">{resourceCardsData[key].count}</div>
+              <div className="card-type">{resourceCardsData[key].msgKey}</div>
+              <div className="card-text">{resourceCardsData[key].textKey}</div>
+            </div>
+          )
+        })}
       </div>
     )
   }
---
+++ src-web/components/ApplicationDeploymentPipeline/components/ResourceCardsInformation/utils.js
@@ -0,0 +1,112 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * 5737-E67
+ * (c) Copyright IBM Corporation 2017, 2019. All Rights Reserved.
+ *
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ *******************************************************************************/
+import R from 'ramda'
+import {
+  getResourcesStatusPerChannel,
+  getAllRelatedForList
+} from '../../components/PipelineGrid/utils'
+
+// Method will take in an object and return back the status of related objects
+// returns [completed + unidentified, fail, inprogress + pending]
+// Given the tall count of completed + unidentified, fail, inprogress + pending
+export const getAllDeployablesStatus = list => {
+  const statusPassFailInProgress = [0, 0, 0]
+  if (list && list.items instanceof Array && list.items.length > 0) {
+    list.items.map(item => {
+      // Will return back status as:
+      // [0, 0, 0, 0, 0]
+      // Pass, Fail, InProgress, Pending, Unidentified
+      const status = getResourcesStatusPerChannel(item, false)
+      statusPassFailInProgress[0] =
+        statusPassFailInProgress[0] + status[0] + status[4]
+      statusPassFailInProgress[1] = statusPassFailInProgress[1] + status[1]
+      statusPassFailInProgress[2] =
+        statusPassFailInProgress[2] + status[2] + status[3]
+    })
+  }
+  return statusPassFailInProgress
+}
+
+export const getNumClusters = (applications, allsubscriptions) => {
+  if (allsubscriptions && allsubscriptions.items) {
+    const subscriptionsInApp = getAllRelatedForList(
+      applications,
+      'subscription'
+    )
+
+    if (subscriptionsInApp && subscriptionsInApp.length > 0) {
+      const subscriptionForSearch = allsubscriptions.items.map(item => {
+        if (item && item.name && item.namespace) {
+          for (var i = 0; i < subscriptionsInApp.length; i++) {
+            const sappitem = subscriptionsInApp[i]
+
+            if (
+              sappitem &&
+              sappitem.name &&
+              sappitem.namespace &&
+              sappitem.name === item.name &&
+              sappitem.namespace === item.namespace
+            )
+              return item
+          }
+        }
+      })
+
+      const removeUndefined = x => x !== undefined
+      const emptyArray = []
+      const removedUndefinedSubscriptions = R.filter(
+        removeUndefined,
+        subscriptionForSearch
+      )
+      const resultList = emptyArray.concat.apply(
+        [],
+        removedUndefinedSubscriptions
+      )
+      return getAllRelatedForList({ items: resultList }, 'cluster').length
+    }
+  }
+
+  return 0
+}
+
+export const getApplicationName = list => {
+  if (
+    list &&
+    list.items instanceof Array &&
+    list.items.length > 0 &&
+    list.items[0].name
+  ) {
+    return list.items[0].name
+  }
+  return ''
+}
+
+export const getSingleApplicationObject = list => {
+  if (list && list.items instanceof Array && list.items.length > 0) {
+    return list.items[0]
+  }
+  return ''
+}
+
+export const getChannelsCountFromSubscriptions = arr => {
+  const channelSet = new Set()
+  if (arr instanceof Array && arr.length > 0) {
+    arr.map(elem => {
+      if (elem && elem.items instanceof Array && elem.items.length > 0) {
+        elem.items.map(subelem => {
+          if (subelem.channel && !subelem._hostingSubscription) {
+            // count only hub subscriptions
+            channelSet.add(subelem.channel)
+          }
+        })
+      }
+    })
+  }
+  return channelSet.size
+}

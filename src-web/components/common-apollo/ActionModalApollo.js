60979eb6b414acafb7b92db50711c4a8cac66316
Valentina Birsan <popescu@ca.ibm.com>
2019-10-25 17:14:19 -0400

    icam and grafana links

--- src-web/components/common-apollo/ActionModalApollo.js
+++ src-web/components/common-apollo/ActionModalApollo.js
@@ -13,7 +13,7 @@ import React from 'react'
 import loadable from 'loadable-components'
 import { GET_ACTION_MODAL_STATE } from '../../apollo-client/queries/StateQueries'
 import { Query } from 'react-apollo'
-import { getIcamLinkForSubscription } from '../common/ResourceDetails/utils'
+import { getPerfmonLinkForApp } from '../common/ResourceDetails/utils'
 
 let RemoveResourceModal
 let LabelEditingModal
@@ -44,7 +44,7 @@ class ActionModalApollo extends React.PureComponent {
       )
     }
     case 'table.actions.applications.perfmon': {
-      const link = getIcamLinkForSubscription(data._uid, data.clusterName)
+      const link = getPerfmonLinkForApp(data._uid, data.clusterName)
 
       window.open(link, '_blank')
       return null

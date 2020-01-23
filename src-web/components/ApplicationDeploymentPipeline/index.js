0475733166ebe015a63b76f8b5aab7194ab73d8f
Feng Xiang <fxiang@ca.ibm.com>
2020-01-23 14:13:46 -0500

    Clean up commented code

--- src-web/components/ApplicationDeploymentPipeline/index.js
+++ src-web/components/ApplicationDeploymentPipeline/index.js
@@ -285,7 +285,6 @@ class ApplicationDeploymentPipeline extends React.Component {
       )
       this.setState({ intervalId: intervalId })
     }
-    //this.reload()
   }
 
   componentDidMount() {}

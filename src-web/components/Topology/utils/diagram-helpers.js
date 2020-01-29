b89166d8ed2ed91406782b1fde5188394d2d299e
Feng Xiang <fxiang@ca.ibm.com>
2020-01-29 14:58:53 -0500

    Fix topology search issues

--- src-web/components/Topology/utils/diagram-helpers.js
+++ src-web/components/Topology/utils/diagram-helpers.js
@@ -15,17 +15,20 @@ import ReactDOMServer from 'react-dom/server'
 * UI helpers to help with data transformations
 * */
 
-export const getWrappedNodeLabel = (label, width, rows=3) => {
+export const getWrappedNodeLabel = (label, width, rows = 3) => {
   // if too long, add elipse and split the rest
   const ip = /\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}$/.exec(label)
   if (ip) {
     label = label.substr(0, ip.index) + '\n' + ip[0]
   } else {
-    if (label.length>width*rows) {
-      if (rows===2) {
-        label = label.substr(0, width)+ '..\n' + label.substr(-width)
+    if (label.length > width * rows) {
+      if (rows === 2) {
+        label = label.substr(0, width) + '..\n' + label.substr(-width)
       } else {
-        label = splitLabel(label.substr(0, width*2), width, rows-1) + '..\n' +  label.substr(-width)
+        label =
+          splitLabel(label.substr(0, width * 2), width, rows - 1) +
+          '..\n' +
+          label.substr(-width)
       }
     } else {
       label = splitLabel(label, width, rows)
@@ -35,7 +38,7 @@ export const getWrappedNodeLabel = (label, width, rows=3) => {
 }
 
 const splitLabel = (label, width, rows) => {
-  let line=''
+  let line = ''
   const lines = []
   const parts = label.split(/([^A-Za-z0-9])+/)
   let remaining = label.length
@@ -50,20 +53,20 @@ const splitLabel = (label, width, rows) => {
 
     // if next label part puts it over width split it
     if (parts.length) {
-      if (line.length+parts[0].length > width) {
+      if (line.length + parts[0].length > width) {
         remaining -= line.length
-        if (remaining>width) {
-          if (rows===2) {
+        if (remaining > width) {
+          if (rows === 2) {
             // if pentulitmate row do a hard break
             const split = parts[0]
             const idx = width - line.length
-            line += split.substr(0,idx)
+            line += split.substr(0, idx)
             parts[0] = split.substr(idx)
           }
         }
         lines.push(line)
         line = ''
-        rows-=1
+        rows -= 1
       }
     } else {
       // nothing left, push last line
@@ -72,9 +75,9 @@ const splitLabel = (label, width, rows) => {
   } while (parts.length)
 
   // pull last line in if too short
-  if (lines.length>1) {
+  if (lines.length > 1) {
     let lastLine = lines.pop()
-    if (lastLine.length<=2) {
+    if (lastLine.length <= 2) {
       lastLine = lines.pop() + lastLine
     }
     lines.push(lastLine)
@@ -84,44 +87,67 @@ const splitLabel = (label, width, rows) => {
 
 //as scale decreases from max to min, return a counter zoomed value from min to max
 export const counterZoom = (scale, scaleMin, scaleMax, valueMin, valueMax) => {
-  if (scale>=scaleMax) {
+  if (scale >= scaleMax) {
     return valueMin
-  } else if (scale<=scaleMin) {
+  } else if (scale <= scaleMin) {
     return valueMax
   }
-  return valueMin + (1-((scale-scaleMin)/(scaleMax-scaleMin))) * (valueMax-valueMin)
+  return (
+    valueMin +
+    (1 - (scale - scaleMin) / (scaleMax - scaleMin)) * (valueMax - valueMin)
+  )
 }
 
-
-export const getTooltip = (tooltips) => {
+export const getTooltip = tooltips => {
   return ReactDOMServer.renderToStaticMarkup(
     <React.Fragment>
-      {tooltips.map(({name, value, href, target='', rel=''}) => {
-        return (<div key={Math.random()} >
-          <span className='label'>{name}:  </span>
-          {href ?
-            <a className='link' href={href} target={target} rel={rel} >{value}</a>
-            : <span className='value'>{value}</span>}
-        </div>)
-      })}
-    </React.Fragment>)
+      {tooltips.map(
+        ({
+          name,
+          value,
+          href,
+          target = '_blank',
+          rel = 'noopener noreferrer'
+        }) => {
+          return (
+            <div key={Math.random()}>
+              <span className="label">{name}: </span>
+              {href ? (
+                <a className="link" href={href} target={target} rel={rel}>
+                  {value}
+                </a>
+              ) : (
+                <span className="value">{value}</span>
+              )}
+            </div>
+          )
+        }
+      )}
+    </React.Fragment>
+  )
 }
 
-export const getHashCode = (str) => {
-  let hash = 0, i, chr
+export const getHashCode = str => {
+  let hash = 0,
+      i,
+      chr
   for (i = 0; i < str.length; i++) {
-    chr   = str.charCodeAt(i)
-    hash  = ((hash << 5) - hash) + chr
+    chr = str.charCodeAt(i)
+    hash = (hash << 5) - hash + chr
     hash |= 0
   }
   return hash
 }
 
-export const getStoredObject = (storageKey) => {
+export const getStoredObject = storageKey => {
   try {
     storageKey = `${storageKey} ${window.location.href}`
     const sessionObject = JSON.parse(sessionStorage.getItem(storageKey))
-    if (sessionObject && sessionObject.expiresAt && sessionObject.expiresAt > Date.now()) {
+    if (
+      sessionObject &&
+      sessionObject.expiresAt &&
+      sessionObject.expiresAt > Date.now()
+    ) {
       return sessionObject.sessionData
     } else {
       sessionStorage.removeItem(storageKey)
@@ -132,11 +158,11 @@ export const getStoredObject = (storageKey) => {
   return null
 }
 
-export const saveStoredObject = (storageKey, object, expiring=60) => {
+export const saveStoredObject = (storageKey, object, expiring = 60) => {
   try {
     storageKey = `${storageKey} ${window.location.href}`
     const sessionObject = {
-      expiresAt: Date.now() + expiring*60*1000, // expire in 60 minutes
+      expiresAt: Date.now() + expiring * 60 * 1000, // expire in 60 minutes
       sessionData: object
     }
     sessionStorage.setItem(storageKey, JSON.stringify(sessionObject))
@@ -144,4 +170,3 @@ export const saveStoredObject = (storageKey, object, expiring=60) => {
     // no privileges
   }
 }
-
--- src-web/components/Topology/viewer/defaults/tooltips.js
+++ src-web/components/Topology/viewer/defaults/tooltips.js
@@ -11,47 +11,61 @@
 import msgs from '../../../../../nls/platform.properties'
 import _ from 'lodash'
 
-export const getNodeTooltips = (searchUrl, node, locale ) => {
+export const getNodeTooltips = (searchUrl, node, locale) => {
   const tooltips = []
-  const {name, namespace, type, layout={}} = node
+  const { name, namespace, type, layout = {} } = node
   const { hasPods, pods } = layout
-  if (type==='pod') {
+  if (type === 'pod') {
     addPodTooltips(node, tooltips, searchUrl, locale)
   } else {
-    let kind=undefined
+    let kind = undefined
     switch (type) {
     case 'persistent_volume':
-      kind='persistentvolume'
+      kind = 'persistentvolume'
       break
     case 'persistent_volume_claim':
-      kind='persistentvolumeclaim'
+      kind = 'persistentvolumeclaim'
+      break
+    case 'rules':
+      kind = 'placementrule'
       break
     default:
-      kind=type
+      kind = type
       break
     }
-    const href = searchUrl && kind ? `${searchUrl}?filters={"textsearch":"kind:${kind} name:${name}"}` : undefined
-    tooltips.push({name:getType(type, locale), value:name, href})
+    const href =
+      searchUrl && kind
+        ? `${searchUrl}?filters={"textsearch":"kind:${kind} name:${name}"}`
+        : undefined
+    tooltips.push({ name: getType(type, locale), value: name, href })
     if (hasPods) {
-      pods.forEach(pod=>{
+      pods.forEach(pod => {
         addPodTooltips(pod, tooltips, searchUrl, locale)
       })
     }
   }
   if (namespace) {
     const href = `${searchUrl}?filters={"textsearch":"kind:namespace name:${namespace}"}`
-    tooltips.push({name:msgs.get('resource.namespace', locale), value:namespace, href})
+    tooltips.push({
+      name: msgs.get('resource.namespace', locale),
+      value: namespace,
+      href
+    })
   }
   return tooltips
 }
 
 function addPodTooltips(pod, tooltips, searchUrl, locale) {
-  const {name} = pod
-  const href = searchUrl ? `${searchUrl}?filters={"textsearch":"kind:pod name:${name}"}` : null
-  tooltips.push({name:getType('pod', locale), value:name, href})
+  const { name } = pod
+  const href = searchUrl
+    ? `${searchUrl}?filters={"textsearch":"kind:deployment name:${name}"}&showrelated=pod`
+    : null
+  tooltips.push({ name: getType('pod', locale), value: name, href })
 }
 
 function getType(type, locale) {
   const nlsType = msgs.get(`resource.${type}`, locale)
-  return !nlsType.startsWith('!resource.') ? nlsType : _.capitalize(_.startCase(type))
+  return !nlsType.startsWith('!resource.')
+    ? nlsType
+    : _.capitalize(_.startCase(type))
 }

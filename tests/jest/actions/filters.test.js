6203d83779389d45374fd3c27c82eb7692ee2a82
Valentina Birsan <popescu@ca.ibm.com>
2020-03-04 16:52:46 -0500

    actions junits

---
+++ tests/jest/actions/filters.test.js
@@ -0,0 +1,53 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
+ *
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ *******************************************************************************/
+
+/*
+For a given input, a selector should always produce the same output.
+ */
+import {
+  combineFilters,
+  receiveFiltersSuccess,
+  updateResourceFilters
+} from "../../../src-web/actions/filters";
+
+describe("combineFilters ", () => {
+  it("should return a empty filters", () => {
+    const input = [];
+    const expectedValue = { filter: { resourceFilter: [] } };
+    expect(combineFilters(input)).toEqual(expectedValue);
+  });
+
+  it("should return filters succcess", () => {
+    const resourceType = {
+      name: "QueryApplications",
+      list: "QueryApplicationList"
+    };
+    const input = ["test"];
+    const expectedValue = {
+      filters: { clusterLabels: [], clusterNames: [] },
+      resourceType: ["test"],
+      status: "DONE",
+      type: "RESOURCE_FILTERS_RECEIVE_SUCCESS"
+    };
+    expect(receiveFiltersSuccess(resourceType, input)).toEqual(expectedValue);
+  });
+
+  it("should return updated filters", () => {
+    const resourceType = {
+      name: "QueryApplications",
+      list: "QueryApplicationList"
+    };
+    const input = ["test"];
+    const expectedValue = {
+      resourceName: "QueryApplications",
+      selectedFilters: ["test"],
+      type: "RESOURCE_FILTERS_UPDATE"
+    };
+    expect(updateResourceFilters(resourceType, input)).toEqual(expectedValue);
+  });
+});

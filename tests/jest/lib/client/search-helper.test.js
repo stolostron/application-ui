3a7ef40825238413aadcbffcbf98f3c35b7edf59
Harry Li <harli@redhat.com>
2020-03-23 15:34:08 -0400

    fixing search helper test

--- tests/jest/lib/client/search-helper.test.js
+++ tests/jest/lib/client/search-helper.test.js
@@ -8,12 +8,34 @@ import {
 } from "../../../../lib/client/search-helper";
 
 describe("convertStringToQuery", () => {
-  it("empty string", () => {
+  it("empty string - application", () => {
     const input = "kind:application";
     const expectedOutput = {
       keywords: [],
       filters: { property: "kind", values: ["application"] },
-      relatedKinds: ["subscription", "pod", "cluster"]
+      relatedKinds: ["placementRule"]
+    };
+    const output = convertStringToQuery(input);
+    expect(output).toEqual(expectedOutput);
+  });
+
+  it("empty string - channel", () => {
+    const input = "kind:channel";
+    const expectedOutput = {
+      keywords: [],
+      filters: { property: "kind", values: ["channel"] },
+      relatedKinds: ["subscription"]
+    };
+    const output = convertStringToQuery(input);
+    expect(output).toEqual(expectedOutput);
+  });
+
+  it("empty string - subscription", () => {
+    const input = "kind:subscription";
+    const expectedOutput = {
+      keywords: [],
+      filters: { property: "kind", values: ["application"] },
+      relatedKinds: ["placementrule", "deployable", "application"]
     };
     const output = convertStringToQuery(input);
     expect(output).toEqual(expectedOutput);
@@ -22,15 +44,12 @@ describe("convertStringToQuery", () => {
 
 describe("formatNumber", () => {
   it("0", () => {
-    expect(formatNumber(123)).toEqual("0");
+    expect(formatNumber(123)).toEqual(0);
   });
   it("9999", () => {
-    expect(formatNumber(9999)).toEqual("9k");
+    expect(formatNumber(9999)).toEqual("9.9k");
   });
   it("123", () => {
-    expect(formatNumber(123)).toEqual("123");
-  });
-  it("9999", () => {
-    expect(formatNumber(9999)).toEqual("9k");
+    expect(formatNumber(123)).toEqual(123);
   });
 });

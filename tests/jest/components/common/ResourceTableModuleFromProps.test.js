f0cbe547a49ae2d4ea75e057aa593b28cb14db6f
Valentina Birsan <popescu@ca.ibm.com>
2020-04-01 16:04:01 -0400

    add event testcase

--- tests/jest/components/common/ResourceTableModuleFromProps.test.js
+++ tests/jest/components/common/ResourceTableModuleFromProps.test.js
@@ -11,6 +11,7 @@ const React = require("../../../../node_modules/react");
 
 import ResourceTableModuleFromProps from "../../../../src-web/components/common/ResourceTableModuleFromProps";
 
+import { mount } from "enzyme";
 import renderer from "react-test-renderer";
 import { Provider } from "react-redux";
 import configureMockStore from "redux-mock-store";
@@ -42,5 +43,30 @@ describe("ResourceTableModuleFromProps", () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("ResourceTableModuleFromProps renders correctly with data on single app, on click.", () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <Provider store={storeApp}>
+          <ResourceTableModuleFromProps
+            staticResourceData={staticResourceData}
+            definitionsKey={definitionsKey}
+            resourceData={QueryApplicationList}
+            loading={false}
+          />
+        </Provider>
+      </BrowserRouter>
+    );
+    /*
+    wrapper
+      .find(".bx--search-input")
+      .simulate("change");
+
+    wrapper
+      .find(".bx--table-sort-v2")
+      .find(".bx--table-sort-v2--ascending").at(0)
+      .simulate("click");
+*/
   });
 });

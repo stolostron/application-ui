e97134fb5d266c4f9483d00b6199522ab1119e69
Valentina Birsan <popescu@ca.ibm.com>
2020-04-06 13:45:58 -0400

    enable coverage for all files

--- jest.config.js
+++ jest.config.js
@@ -17,32 +17,7 @@ const tapReporter = [
 
 const jestConfig = {
   collectCoverage: true,
-  collectCoverageFrom: [
-    'src-web/**/*.{js,jsx}',
-    '**/src-web/**/*.{js}',
-    '!**/src-web/root.js',
-    '!**/src-web/app.js',
-    '!**/src-web/providers/*.*',
-    '!**/src-web/pages/routes/*',
-    '!**/node_modules/**',
-    '!**/vendor/**',
-    '!**/tmp/**',
-    '!**/src-web/components/Topology/viewer/ChannelControl.js',
-    '!**/src-web/components/Topology/viewer/RefreshTimeSelect.js',
-    '!**/src-web/components/Topology/viewer/ResourceFilterModule.js',
-    '!**/src-web/components/Topology/viewer/SearchName.js',
-    '!**/src-web/components/Topology/viewer/TypeFilterBar.js',
-    '!**/src-web/components/Topology/viewer/defaults/index.js',
-    '!**/src-web/components/Topology/viewer/defaults/shapes.js',
-    '!**/src-web/components/Topology/viewer/defaults/titles.js',
-    '!**/src-web/components/Topology/viewer/constants.js',
-    '!**/src-web/components/Topology/index.js',
-    '!**/src-web/components/ApplicationTopologyModule/index.js',
-    '!**/src-web/components/Topology/viewer/layouts/policy.js',
-    '!**/src-web/components/Topology/viewer/layouts/application.js',
-    '!**/src-web/components/Topology/viewer/layouts/auto.js',
-    '!**/src-web/components/Topology/viewer/layouts/index.js'
-  ],
+  collectCoverageFrom: ['src-web/**/*.{js,jsx}'],
   coverageDirectory: './test-output/coverage',
   coverageReporters: [
     'json-summary',

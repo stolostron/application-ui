33aa0382796338f1d3e736b6aa99bd6c43b79770
Valentina Birsan <popescu@ca.ibm.com>
2020-04-07 14:23:19 -0400

    check RH copyright on new files

--- copyright-check.sh
+++ copyright-check.sh
@@ -1,9 +1,13 @@
+###############################################################################
 # Licensed Materials - Property of IBM
-# (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
-# Note to U.S. Government Users Restricted Rights:
-# Use, duplication or disclosure restricted by GSA ADP Schedule
-# Contract with IBM Corp.
+# (c) Copyright IBM Corporation 2017, 2019. All Rights Reserved.
+# US Government Users Restricted Rights - Use, duplication or disclosure 
+# restricted by GSA ADP Schedule Contract with IBM Corp.
 # Copyright (c) 2020 Red Hat, Inc.
+#
+# Contributors:
+#  IBM Corporation - initial API and implementation
+###############################################################################
 
 #!/bin/bash
 

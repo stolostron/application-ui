fa982d393346596dc101d51c294ac2d38d5bde53
Richie Piya <rpiyasirisilp@gmail.com>
2020-04-15 11:10:55 -0400

    replace IBM and RH fonts with refs from common header

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 /*******************************************************************************
  * Licensed Materials - Property of IBM
  * (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
- *
+ * Copyright (c) 2020 Red Hat, Inc.
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  *******************************************************************************/
@@ -75,15 +75,11 @@ module.exports = {
             {
               loader: "sass-loader?sourceMap",
               options: {
-                data: `$font-path: "${config.get("contextPath")}/fonts";`
+                data: `$font-path: "${config.get("contextPath")}/header/fonts";`
               }
             }
           ]
         })
-      },
-      {
-        test: /\.woff2?$/,
-        loader: "file-loader?name=fonts/[name].[ext]"
       },
       {
         test: /\.properties$/,
@@ -170,8 +166,7 @@ module.exports = {
             destination: "public/graphics"
           },
           { source: "graphics/*.svg", destination: "public/graphics" },
-          { source: "graphics/*.png", destination: "public/graphics" },
-          { source: "fonts", destination: "public/fonts" }
+          { source: "graphics/*.png", destination: "public/graphics" }
         ]
       }
     }),

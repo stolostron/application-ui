ec1ffe76f1568cf3dcd9299ebe726d5e74ba0739
sahare <sahare@redhat.com>
2020-04-21 16:02:41 -0400

    Revert "fix lint error"
    This reverts commit 97f6089d8d7a0d8ea30d41641d339942ff9163bb.

--- src-web/actions/common.js
+++ src-web/actions/common.js
@@ -318,11 +318,11 @@ export const clearIncidents = resourceType => {
   }
 }
 
-const getGenericResource = (resourceType, variables) => {
+export const fetchIncidents = (resourceType, namespace, name) => {
   return dispatch => {
     dispatch(requestResource(resourceType))
     return apolloClient
-      .getResource(resourceType, variables)
+      .getResource(resourceType, { namespace, name })
       .then(response => {
         if (response.errors) {
           return dispatch(
@@ -340,14 +340,32 @@ const getGenericResource = (resourceType, variables) => {
   }
 }
 
-export const fetchIncidents = (resourceType, namespace, name) => {
-  return getGenericResource(resourceType, { namespace, name })
-}
-
 export const fetchNamespace = (resourceType, namespace) => {
-  return getGenericResource(resourceType, { namespace })
+  return dispatch => {
+    dispatch(requestResource(resourceType))
+    return apolloClient
+      .getResource(resourceType, { namespace })
+      .then(response => {
+        if (response.errors) {
+          return dispatch(
+            receiveResourceError(response.errors[0], resourceType)
+          )
+        }
+        return dispatch(
+          receiveResourceSuccess(
+            { items: lodash.cloneDeep(response.data.items) },
+            resourceType
+          )
+        )
+      })
+      .catch(err => {
+        dispatch(receiveResourceError(err, resourceType))
+      })
+  }
 }
 
+//used only by the app topo; should be removed
+/*
 export const editResource = (
   resourceType,
   namespace,
@@ -377,6 +395,19 @@ export const editResource = (
     })
 }
 
+export const removeResource = (resourceType, vars) => async dispatch => {
+  dispatch(delResource(resourceType))
+  try {
+    const response = await apolloClient.remove(resourceType, vars)
+    if (response.errors) {
+      return dispatch(receiveDelError(response.errors, resourceType))
+    }
+    dispatch(receiveDelResource(response, resourceType, vars))
+  } catch (err) {
+    return dispatch(receiveDelError(err, resourceType))
+  }
+}
+*/
 export const updateSecondaryHeader = (title, tabs, breadcrumbItems, links) => ({
   type: Actions.SECONDARY_HEADER_UPDATE,
   title,
@@ -482,5 +513,26 @@ export const createResources = (resourceType, resourceJson) => {
       }
       return result
     })
+  }
+}
+
+export const createResource = (resourceType, variables) => {
+  return dispatch => {
+    dispatch(postResource(resourceType))
+    return apolloClient
+      .createResource(resourceType, variables)
+      .then(response => {
+        if (response.errors) {
+          return dispatch(receivePostError(response.errors[0], resourceType))
+        }
+
+        return dispatch(
+          receivePostResource(
+            lodash.cloneDeep(response.data.setHelmRepo),
+            resourceType
+          )
+        )
+      })
+      .catch(err => dispatch(receivePostError(err, resourceType)))
   }
 }

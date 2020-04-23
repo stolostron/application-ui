a1534cb90f0dda36bdf717f4a668d0c03dcffd33
sahare <sahare@redhat.com>
2020-04-23 17:01:11 -0400

    Fix link helper bug (#205)

--- src-web/components/Topology/viewer/helpers/linkHelper.js
+++ src-web/components/Topology/viewer/helpers/linkHelper.js
@@ -180,11 +180,7 @@ export default class LinkHelper {
       }
     })
 
-    links.transition(transition).style(() => {
-      return {
-        opacity: 1.0
-      }
-    })
+    links.transition(transition).style(('opacity': 1.0))
 
     // move line labels
     if (this.diagramOptions.showLineLabels) {

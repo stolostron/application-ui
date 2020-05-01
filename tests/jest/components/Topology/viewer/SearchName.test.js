b59f589390f2d5e87a1085395a367ec51c7e0f12
Feng Xiang <fxiang@redhat.com>
2020-05-01 17:38:25 -0400

    Add more SearchName testing

--- tests/jest/components/Topology/viewer/SearchName.test.js
+++ tests/jest/components/Topology/viewer/SearchName.test.js
@@ -3,23 +3,27 @@
  *******************************************************************************/
 "use strict";
 
-import React from "react";
 import Search from "../../../../../src-web/components/Topology/viewer/SearchName";
 import renderer from "react-test-renderer";
 
-const emptySearchName = "";
+const testSearchName = "test";
 const locale = "en-US";
 
-describe("SearchName empty search", () => {
+describe("SearchName search", () => {
   let btn = document.createElement("a");
-  btn.setAttribute("className", "bx--search-close");
+  btn.innerHTML = "<button className='bx--search-close' />";
+  btn.className = "bx--search-close";
+  document.body.appendChild(btn);
   const props = {
-    searchName: emptySearchName,
+    searchName: testSearchName,
     onNameSearch: jest.fn(),
     locale: locale
   };
+
   const searchInst = new Search(props);
-  searchInst.setNameSearchRef(btn);
+  searchInst.setNameSearchRef(document);
+  searchInst.componentDidMount();
+  searchInst.handleSearch({ target: { value: "test" } });
   it("render as expected", () => {
     const component = renderer.create(searchInst.render());
     expect(component.toJSON()).toMatchSnapshot();

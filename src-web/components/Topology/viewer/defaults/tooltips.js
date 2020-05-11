fa0a0e18f029f0640f0c7d7a0b5ac0f3dedc8b93
Valentina Birsan <vbirsan@vbirsan@redhat.com-mac>
2020-05-11 15:52:49 -0400

    fix code smell

--- src-web/components/Topology/viewer/defaults/tooltips.js
+++ src-web/components/Topology/viewer/defaults/tooltips.js
@@ -8,72 +8,35 @@
  *******************************************************************************/
 'use strict'
 
+import R from 'ramda'
 import msgs from '../../../../../nls/platform.properties'
 import _ from 'lodash'
 
 export const getNodeTooltips = (searchUrl, node, locale) => {
   const tooltips = []
-  const { name, namespace, type, layout = {} } = node
-  const { hasPods, pods } = layout
-  if (type === 'pod') {
-    addPodTooltips(node, tooltips, searchUrl, locale)
-  } else if (type === 'package') {
+  const { name, namespace, type } = node
+
+  if (!searchUrl || !type || type === '' || type === 'package') {
     return []
+  }
+
+  let kind = R.replace(/_/g, '')(type) //remove globally _ occurences
+  if (kind === 'rules') {
+    kind = 'placementrule'
+  }
+
+  if (kind === 'cluster') {
+    addClustersTooltip(node, searchUrl, name, tooltips, locale)
   } else {
-    let kind = undefined
-    switch (type) {
-    case 'persistent_volume':
-      kind = 'persistentvolume'
-      break
-    case 'persistent_volume_claim':
-      kind = 'persistentvolumeclaim'
-      break
-    case 'rules':
-      kind = 'placementrule'
-      break
-    default:
-      kind = type
-      break
-    }
-    var href
-    if (searchUrl && kind) {
-      if (kind === 'cluster' && name.includes(',')) {
-        const clusterList = name.replace(/\s/g, '')
-        href = `${searchUrl}?filters={"textsearch":"kind:${kind} name:${clusterList}"}`
-      } else {
-        const searchName = kind === 'helmrelease' ? name : `name:${name}`
-        const searchNS = namespace ? `namespace:${namespace}` : ''
-        href = namespace
-          ? `${searchUrl}?filters={"textsearch":"kind:${kind} ${searchName} ${searchNS}"}`
-          : `${searchUrl}?filters={"textsearch":"kind:${kind} ${searchName}"}`
-      }
-    }
+    let href = ''
+    const searchName = kind === 'helmrelease' ? name : `name:${name}`
+    href = namespace
+      ? `${searchUrl}?filters={"textsearch":"kind:${kind} ${searchName} namespace:${namespace}"}`
+      : `${searchUrl}?filters={"textsearch":"kind:${kind} ${searchName}"}`
+
     tooltips.push({ name: getType(type, locale), value: name, href })
-    if (hasPods) {
-      pods.forEach(pod => {
-        addPodTooltips(pod, tooltips, searchUrl, locale)
-      })
-    }
   }
-  if (type === 'cluster') {
-    const label = msgs.get('tooltips.console', locale)
-    const clusters = _.get(node, 'specs.clusters')
-    if (clusters) {
-      clusters.forEach(n => {
-        if (n.consoleURL) {
-          const href = n.consoleURL
-          tooltips.push({
-            name: label,
-            value: `${n.metadata.name}-console`,
-            href
-          })
-        }
-      })
-    } else {
-      const href = _.get(node, 'specs.cluster.consoleURL')
-      tooltips.push({ name: label, value: `${name}-console`, href })
-    }
-  }
+
   if (namespace) {
     const href = `${searchUrl}?filters={"textsearch":"kind:namespace name:${namespace}"}`
     tooltips.push({
@@ -82,18 +45,40 @@ export const getNodeTooltips = (searchUrl, node, locale) => {
       href
     })
   }
+
   return tooltips
 }
 
-function addPodTooltips(pod, tooltips, searchUrl, locale) {
-  const { name } = pod
-  const href = searchUrl
-    ? `${searchUrl}?filters={"textsearch":"kind:deployment name:${name}"}&showrelated=pod`
-    : null
-  tooltips.push({ name: getType('pod', locale), value: name, href })
+const addClustersTooltip = (node, searchUrl, name, tooltips, locale) => {
+  const kind = 'cluster'
+  let href = `${searchUrl}?filters={"textsearch":"kind:${kind} name:${name}"}`
+  if (name.includes(',')) {
+    const clusterList = name.replace(/\s/g, '')
+    href = `${searchUrl}?filters={"textsearch":"kind:${kind} name:${clusterList}"}`
+  }
+
+  tooltips.push({ name: getType(kind, locale), value: name, href })
+
+  const label = msgs.get('tooltips.console', locale)
+  const clusters = _.get(node, 'specs.clusters')
+  if (clusters) {
+    clusters.forEach(n => {
+      if (n.consoleURL) {
+        const href = n.consoleURL
+        tooltips.push({
+          name: label,
+          value: `${n.metadata.name}-console`,
+          href
+        })
+      }
+    })
+  } else {
+    const href = _.get(node, 'specs.cluster.consoleURL')
+    tooltips.push({ name: label, value: `${name}-console`, href })
+  }
 }
 
-function getType(type, locale) {
+const getType = (type, locale) => {
   const nlsType = msgs.get(`resource.${type}`, locale)
   return !nlsType.startsWith('!resource.')
     ? nlsType

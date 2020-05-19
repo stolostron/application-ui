6493a920209bb976b27ceec7e61490207b122059
jswanke <jswanke@JSWANKE-W10E.win.redhat.com>
2020-05-19 10:26:58 -0400

    exception icons

--- src-web/components/common/YamlEditor.js
+++ src-web/components/common/YamlEditor.js
@@ -20,6 +20,7 @@ import {
 import 'monaco-editor/esm/vs/editor/editor.all.js'
 import 'monaco-editor/esm/vs/editor/standalone/browser/quickOpen/quickCommand.js'
 import 'monaco-editor/esm/vs/basic-languages/yaml/yaml.contribution.js'
+import { parse } from '../../../lib/client/design-helper'
 import config from '../../../lib/shared/config'
 
 
@@ -52,7 +53,9 @@ window.MonacoEnvironment = {
 class IsomorphicEditor extends React.Component {
 
   static propTypes = {
+    handleParsingError: PropTypes.func,
     setEditor: PropTypes.func,
+    validator: PropTypes.func,
   }
 
   constructor(props) {
@@ -90,7 +93,7 @@ class IsomorphicEditor extends React.Component {
   }
 
   editorDidMount(editor, monaco) {
-    const { setEditor } = this.props
+    const { setEditor, handleParsingError, validator } = this.props
     editor.layout()
     editor.focus()
     editor.monaco = monaco
@@ -108,6 +111,48 @@ class IsomorphicEditor extends React.Component {
     }
 
     monaco.editor.setModelLanguage(editor.getModel(), 'yaml')
+
+    // syntax errors
+    if (handleParsingError) {
+      editor.getModel().onDidChangeContent(() => {
+        const yaml = editor.getValue()
+        const { exceptions } =
+          yaml.length > 0
+            ? parse(yaml, validator, this.context.locale)
+            : { exceptions: [] }
+        const decorationList = []
+        exceptions.forEach(({row, text})=>{
+          decorationList.push({
+            range: new monaco.Range(row+1, 0, row+1, 132),
+            options: {
+              isWholeLine: true,
+              glyphMarginClassName: 'errorDecoration',
+              glyphMarginHoverMessage: {value: text},
+              minimap: {color: 'red' , position:1}
+            }
+          })
+        })
+        exceptions.forEach(({row, column})=>{
+          decorationList.push({
+            range: new monaco.Range(row+1, column-6, row+1, column+6),
+            options: {
+              className: 'squiggly-error',
+            }
+          })
+        })
+        editor.decorations = editor.deltaDecorations(editor.decorations, decorationList)
+        let reason = exceptions
+          .map(({ text }) => {
+            return text
+          })
+          .join('; ')
+        if (reason.length > 200) {
+          reason = `${reason.substr(0, 200)}...`
+        }
+        handleParsingError(exceptions.length > 0 ? { reason } : null)
+      })
+    }
+
   }
 
   render = () => <MonacoEditor
@@ -118,40 +163,43 @@ class IsomorphicEditor extends React.Component {
   />
 }
 
-const YamlEditor = ({ onYamlChange, setEditor, yaml, width='49.5vw', height='40vh', readOnly=false }) => (
-  <div className="yamlEditorContainer">
-    <IsomorphicEditor
-      key={!yaml ? 'loading' : 'loaded'}
-      theme="console"
-      language="yaml"
-      width={width}
-      height={height}
-      onChange={onYamlChange}
-      fontSize={12}
-      value={yaml}
-      options={{
-        readOnly,
-        wordWrap: 'wordWrapColumn',
-        wordWrapColumn: 132,
-        wordWrapMinified: false,
-        scrollBeyondLastLine: false,
-        smoothScrolling: true,
-        glyphMargin: true,
-        tabSize: 2,
-        scrollbar: {
-          verticalScrollbarSize: 17,
-          horizontalScrollbarSize: 17,
-        }
-      }}
-      setEditor={setEditor}
+const YamlEditor = ({ onYamlChange, setEditor, yaml,
+  validator, handleParsingError,  width='49.5vw', height='40vh', readOnly=false }) => (
+    <div className="yamlEditorContainer">
+      <IsomorphicEditor
+        key={!yaml ? 'loading' : 'loaded'}
+        theme="console"
+        language="yaml"
+        width={width}
+        height={height}
+        onChange={onYamlChange}
+        fontSize={12}
+        value={yaml}
+        validator={validator}
+        handleParsingError={handleParsingError}
+        options={{
+          readOnly,
+          wordWrapMinified: false,
+          scrollBeyondLastLine: false,
+          smoothScrolling: true,
+          glyphMargin: true,
+          tabSize: 2,
+          scrollbar: {
+            verticalScrollbarSize: 17,
+            horizontalScrollbarSize: 17,
+          }
+        }}
+        setEditor={setEditor}
     />
-  </div>)
+    </div>)
 
 YamlEditor.propTypes = {
+  handleParsingError: PropTypes.func,
   height: PropTypes.string,
   onYamlChange: PropTypes.func,
   readOnly: PropTypes.bool,
   setEditor: PropTypes.func,
+  validator: PropTypes.func,
   width: PropTypes.string,
   yaml: PropTypes.oneOfType([PropTypes.object, PropTypes.string]),
 }

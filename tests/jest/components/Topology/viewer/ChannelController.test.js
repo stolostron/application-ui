18ea1e72f6efa275e2ec138bfab05b990a564061
Valentina Birsan <vbirsan@vbirsan@redhat.com-mac>
2020-06-03 18:03:32 -0400

    add testcases

---
+++ tests/jest/components/Topology/viewer/ChannelController.test.js
@@ -0,0 +1,40 @@
+/*******************************************************************************
+ * Licensed Materials - Property of IBM
+ * (c) Copyright IBM Corporation 2018, 2019. All Rights Reserved.
+ * Copyright (c) 2020 Red Hat, Inc.
+ *
+ * US Government Users Restricted Rights - Use, duplication or disclosure
+ * restricted by GSA ADP Schedule Contract with IBM Corp.
+ *******************************************************************************/
+"use strict";
+
+import React from "react";
+import { shallow } from "enzyme";
+
+import ChannelController from "../../../../../src-web/components/Topology/viewer/ChannelControl";
+
+const channelController = {
+  activeChannel: "__ALL__/__ALL__//__ALL__/__ALL__",
+  isChangingChannel: undefined,
+  changeTheChannel: jest.fn,
+  allChannels: [
+    "default/guestbook-app//gbapp-ch/guestbook-app-latest///cassandra-cassandra-service///mysql-wordpress-pd-wordpress-mysql-deployment",
+    "default/guestbook-app//gbapp-ch/guestbook-app-latest///mysql-wordpress-pd-wordpress-mysql-service///staging-elasticsearch-elasticsearch-serviceaccount"
+  ]
+};
+describe("EditorBar components", () => {
+  it("empty", () => {
+    const component = shallow(<ChannelController />);
+    expect(component).toMatchSnapshot();
+  });
+
+  it("default", () => {
+    const component = shallow(
+      <ChannelController
+        channelController={channelController}
+        locale={"en-US"}
+      />
+    );
+    expect(component).toMatchSnapshot();
+  });
+});

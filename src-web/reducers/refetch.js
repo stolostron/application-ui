496e243a848db420d3719edfe49ce83207457faa
Harry Li <harli@redhat.com>
2020-06-03 23:03:48 -0400

    add red hat copyright to reducer

--- src-web/reducers/refetch.js
+++ src-web/reducers/refetch.js
@@ -1,9 +1,5 @@
 /*******************************************************************************
- * Licensed Materials - Property of IBM
- * (c) Copyright IBM Corporation 2017, 2019. All Rights Reserved.
- *
- * US Government Users Restricted Rights - Use, duplication or disclosure
- * restricted by GSA ADP Schedule Contract with IBM Corp.
+ * Copyright (c) 2020 Red Hat, Inc.
  *******************************************************************************/
 
 import { REFETCH_INTERVAL_UPDATE } from '../actions'

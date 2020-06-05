a374977cdddf6ef0555f82345e6cfa132bb80b8a
Harry Li <harli@redhat.com>
2020-06-05 00:08:22 -0400

    additional test case

--- tests/jest/components/ApplicationTopologyModule/ApplicationTopologyModule.test.js
+++ tests/jest/components/ApplicationTopologyModule/ApplicationTopologyModule.test.js
@@ -2,6 +2,7 @@
  * Licensed Materials - Property of Red Hat
  * Copyright (c) 2020 Red Hat, Inc.
  *******************************************************************************/
+
 jest.mock("../../../../lib/client/apollo-client", () => ({
   getSearchClient: jest.fn(() => {
     return null;
@@ -128,7 +129,7 @@ import ApplicationTopologyModule from "../../../../src-web/components/Applicatio
 
 import renderer from "react-test-renderer";
 import * as reducers from "../../../../src-web/reducers";
-import { mount } from "enzyme";
+import { mount, shallow } from "enzyme";
 
 import { createStore, combineReducers, applyMiddleware, compose } from "redux";
 import thunkMiddleware from "redux-thunk";
@@ -190,6 +191,23 @@ describe("ApplicationTopologyModule with selected node ID", () => {
       )
       .toJSON();
     expect(tree).toMatchSnapshot();
+  });
+
+  it("ApplicationTopologyModule sets new interval value after interval changed", () => {
+    const wrapper = mount(
+      <BrowserRouter>
+        <Provider store={store}>
+          <ApplicationTopologyModule
+            selectedNodeId={nodeID}
+            showExpandedTopology={false}
+            params={params}
+            locale={locale}
+          />
+        </Provider>
+      </BrowserRouter>
+    );
+
+    wrapper.setState({ refetch: { interval: 30000, doRefetch: true } });
   });
 
   const actions = {

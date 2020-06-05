5590c567b4dc5ee6b5a0e5edde8ae1ebf5929ad3
Harry Li <harli@redhat.com>
2020-06-05 09:35:00 -0400

    fix code smell

--- src-web/reducers/refetch.js
+++ src-web/reducers/refetch.js
@@ -7,7 +7,7 @@ import { REFETCH_INTERVAL_UPDATE } from '../actions'
 // get the refetch default from the config file
 import config from '../../lib/shared/config'
 
-export const refetch = (state = {}, action) => {
+export const refetch = (state, action) => {
   switch (action.type) {
   case REFETCH_INTERVAL_UPDATE:
     return action.data

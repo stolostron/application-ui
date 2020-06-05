e2dc6b68fff9f6c68485cb1681b3f483d6fa800d
Harry Li <harli@redhat.com>
2020-06-05 09:13:35 -0400

    fix code smell

--- src-web/actions/refetch.js
+++ src-web/actions/refetch.js
@@ -2,10 +2,10 @@
  * Copyright (c) 2020 Red Hat, Inc.
  *******************************************************************************/
 
-import * as Actions from './index'
+import { REFETCH_INTERVAL_UPDATE } from './index'
 
 // pass the refetch flag and also the interval value
 export const refetchIntervalUpdate = refetch => ({
-  type: Actions.REFETCH_INTERVAL_UPDATE,
+  type: REFETCH_INTERVAL_UPDATE,
   data: refetch
 })

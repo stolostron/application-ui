3084f0fb68ed0a11e7cd354717416b7e475af84d
Harry Li <harli@redhat.com>
2020-06-05 00:32:33 -0400

    fixing code smell

--- src-web/reducers/refetch.js
+++ src-web/reducers/refetch.js
@@ -7,17 +7,14 @@ import { REFETCH_INTERVAL_UPDATE } from '../actions'
 // get the refetch default from the config file
 import config from '../../lib/shared/config'
 
-export const refetch = (
-  state = {
-    interval: config['featureFlags:liveUpdatesPollInterval'],
-    doRefetch: false
-  },
-  action
-) => {
+export const refetch = action => {
   switch (action.type) {
   case REFETCH_INTERVAL_UPDATE:
     return action.data
   default:
-    return state
+    return {
+      interval: config['featureFlags:liveUpdatesPollInterval'],
+      doRefetch: false
+    }
   }
 }

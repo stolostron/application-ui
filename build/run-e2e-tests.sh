cddb6c08e39cd8297e0b97d4f69502e99680855b
Harry Li <harli@redhat.com>
2020-06-26 17:09:22 -0400

    commenting out docker commands to test

--- build/run-e2e-tests.sh
+++ build/run-e2e-tests.sh
@@ -25,17 +25,13 @@ export OC_CLUSTER_URL=$OCP_CLUSTER_URL
 export OC_CLUSTER_USER=$OCP_CLUSTER_USER
 export OC_CLUSTER_PASS=$OCP_CLUSTER_PASS
 
-echo $OCP_CLUSTER_URL
-echo $OCP_CLUSTER_USER
-echo $OCP_CLUSTER_PASS
-
 echo "Running oc/install..."
 make oc/install
 echo "Running oc/login..."
 make oc/login
 
 echo "Running docker network create..."
-docker network create --subnet 10.10.0.0/16 test-network
+#docker network create --subnet 10.10.0.0/16 test-network
 
 # make docker/login
 #export DOCKER_URI=quay.io/open-cluster-management/console-api:latest
@@ -50,16 +46,16 @@ export SERVICEACCT_TOKEN=`${BUILD_HARNESS_PATH}/vendor/oc whoami --show-token`
 echo "SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN"
 
 #application-ui
-docker run --network test-network -d --ip 10.10.0.6 -t -i -p 3000:3000 --name application-ui \
--e NODE_ENV=development \
--e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN \
--e headerUrl=$headerUrl \
--e OAUTH2_REDIRECT_URL=https://localhost:3000/multicloud/auth/callback \
--e hcmUiApiUrl=https://10.10.0.5:4000/hcmuiapi \
--e OAUTH2_CLIENT_ID=multicloudingress \
--e OAUTH2_CLIENT_SECRET=multicloudingresssecret \
--e API_SERVER_URL=$OC_CLUSTER_URL $UI_CURRENT_IMAGE
-docker container ls -a
+#docker run --network test-network -d --ip 10.10.0.6 -t -i -p 3000:3000 --name application-ui \
+#-e NODE_ENV=development \
+#-e SERVICEACCT_TOKEN=$SERVICEACCT_TOKEN \
+#-e headerUrl=$headerUrl \
+#-e OAUTH2_REDIRECT_URL=https://localhost:3000/multicloud/auth/callback \
+#-e hcmUiApiUrl=https://10.10.0.5:4000/hcmuiapi \
+#-e OAUTH2_CLIENT_ID=multicloudingress \
+#-e OAUTH2_CLIENT_SECRET=multicloudingresssecret \
+#-e API_SERVER_URL=$OC_CLUSTER_URL $UI_CURRENT_IMAGE
+#docker container ls -a
 
 fold_end test-setup
 

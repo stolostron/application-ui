b1f4af9374282687d32d19840e7ac223f8d75a54
Harry Li <harli@redhat.com>
2020-06-30 11:14:02 -0400

    take out resource-cleanup.sh - this is not needed

--- tests/start-cypress-tests.sh
+++ tests/start-cypress-tests.sh
@@ -57,9 +57,9 @@ cp -r ./cypress/videos /results/videos
 #   npm run test:slack
 # fi
 
-if [ $CYPRESS_TEST_MODE == "functional" ]; then
-  echo "Cleaning up functional test resources..."
-  sh ./tests/cypress/scripts/resource-cleanup.sh
-fi
+#if [ $CYPRESS_TEST_MODE == "functional" ]; then
+#  echo "Cleaning up functional test resources..."
+#  sh ./tests/cypress/scripts/resource-cleanup.sh
+#fi
 
 exit $testCode

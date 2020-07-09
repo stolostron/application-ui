a7322881034ecc4dc142ee1cde0b9b463e99de17
Harry Li <harli@redhat.com>
2020-07-08 23:50:52 -0400

    setting env variable

--- tests/generate-cypress-yaml.sh
+++ tests/generate-cypress-yaml.sh
@@ -7,5 +7,5 @@ cp test-artifacts-templates/* test-artifacts/
 cd test-artifacts/
 
 # do a find and replace operation to sub in the ID
-sed -i "s/\$JOB_ID/$JOB_ID/" *.yaml
+sed -i "s/\$JOB_ID/$TRAVIS_JOB_ID/" *.yaml
 

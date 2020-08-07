9423be7ffa2202401f65d1b607863d1ebca81b65
Maggie Chen <chenz.4027@gmail.com>
2020-08-07 09:50:39 -0400

    add more test steps

--- tests/cypress/tests/01-create/02-wizard.spec.js
+++ tests/cypress/tests/01-create/02-wizard.spec.js
@@ -8,20 +8,19 @@ const { wizards } = JSON.parse(Cypress.env("TEST_CONFIG"));
 
 describe("create wizard", () => {
   for (const resource in wizards) {
-    const { url, name } = wizards[resource];
+    const { name, url, branch, path } = wizards[resource];
     it(`can be created on resource ${resource}`, () => {
       cy.visit("/multicloud/applications");
       modal.clickSecondary();
       cy.get(".bx--detail-page-header-title-container").should("exist");
       cy.get("#name").type(name);
       cy.get(`#${resource}`).click();
-      cy.get(".creation-view-group-container").within(() => {
-        cy
-          .get("input", { timeout: 20000 })
-          .should("have.attr", "placeholder", "Enter or select Git URL");
-      });
-      //   cy.get("#githubUser", { timeout: 20000 }).type(username);
-      //   cy.get("#githubAccessId", { timeout: 20000 }).type(accessToken);
+      cy.get("#githubURL").type(url);
+      cy.get("#githubBranch").type(branch);
+      cy.get("#githubPath").type(path);
+
+      cy.get("#online-cluster-only-checkbox").click({ force: true });
+      cy.get("#create-button-portal-id").click();
     });
   }
 });

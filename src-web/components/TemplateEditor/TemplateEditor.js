7a424da7fba13181037cb9eee2458e7c6cfc16fa
Valentina Birsan <43010150+birsanv@users.noreply.github.com>
2020-08-18 12:49:25 -0400

    always enable Save (#493)
    Co-authored-by: Valentina Birsan <vbirsan@vbirsan@redhat.com-mac>

--- src-web/components/TemplateEditor/TemplateEditor.js
+++ src-web/components/TemplateEditor/TemplateEditor.js
@@ -1035,7 +1035,6 @@ export default class TemplateEditor extends React.Component {
             id={createBtn}
             onClick={this.handleCreateResource.bind(this)}
             kind={'primary'}
-            disabled={!this.state.hasUndo}
           >
             {msgs.get('button.create', locale)}
           </Button>,

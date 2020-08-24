fd84223f6fbfe25528112549cf19eec74df9590d
Maggie Chen <chenz.4027@gmail.com>
2020-08-24 14:32:21 -0400

    add template

---
+++ tests/cypress/tests/02-validate/03-validate-timewindow.spec.js
@@ -0,0 +1,16 @@
+/** *****************************************************************************
+ * Licensed Materials - Property of Red Hat, Inc.
+ * Copyright (c) 2020 Red Hat, Inc.
+ ****************************************************************************** */
+
+// import { validateResourceTable } from "../../views/application";
+const { wizards } = JSON.parse(Cypress.env("TEST_CONFIG"));
+
+describe("Application", () => {
+  for (const type in wizards) {
+    const { name } = wizards[type];
+    it(`should be validated from the resource table - ${type}: ${name}`, () => {
+      validateResourceTable(name);
+    });
+  }
+});

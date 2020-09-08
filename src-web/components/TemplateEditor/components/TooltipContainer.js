123bccb4c9472e0d37cd5f4638d8536010bfabeb
jswanke <jswanke@JSWANKE-W10E.win.redhat.com>
2020-09-08 09:13:58 -0400

    fix tooltip

--- src-web/components/TemplateEditor/components/TooltipContainer.js
+++ src-web/components/TemplateEditor/components/TooltipContainer.js
@@ -18,7 +18,7 @@ const TooltipContainer = ({ isDisabled, tooltip, children }) => {
       <div className="bx--tooltip__trigger" aria-describedby="definition-tooltip-4">
         {children}
       </div>
-      <div id="definition-tooltip-4" className="bx--tooltip--definition__top" role="tooltip" aria-label={tooltip}>
+      <div id="definition-tooltip-4" className="bx--tooltip--definition__bottom" role="tooltip" aria-label={tooltip}>
         <span className="bx--tooltip__caret"></span>
         <p>{tooltip}</p>
       </div>

9c83525e44907ffdd0a1a5202e2c58b740a43541
Kevin Cormier <kcormier@redhat.com>
2020-09-16 12:23:44 -0400

    Remove hover behaviour from label popover (#596)
    * Remove hover behaviour and adjust tests
    
    * Include count in label when > 1
    Update tests
    
    * Restore close button and add space at top
    Make styles more adjustable
    
    * Add sorting for channels
    
    * Adjust spacing for case with no Git attributes
    
    * Fix z-index
    
    * Update setup-env.sh with correct console-api endpoint

--- src-web/components/common/LabelWithPopover.js
+++ src-web/components/common/LabelWithPopover.js
@@ -1,105 +1,55 @@
 // Copyright (c) 2020 Red Hat, Inc.
 'use strict'
 
-import React, { useState } from 'react'
+import React from 'react'
 import PropTypes from 'prop-types'
-import { Label, Popover } from '@patternfly/react-core'
+import {
+  Divider,
+  Label,
+  Popover,
+  Stack,
+  StackItem
+} from '@patternfly/react-core'
 import resources from '../../../lib/shared/resources'
 
 resources(() => {
   require('../../../scss/label-with-popover.scss')
 })
 
-const LabelWithPopover = ({ children, labelContent, labelIcon }) => {
-  const hoverDelay = 1500
-  const [
-    { popoverOpen, popoverPinnedOpen, justClosed, timeout },
-    setPopoverState
-  ] = useState({
-    popoverOpen: false,
-    popoverPinnedOpen: false,
-    justClosed: false,
-    timeout: null
-  })
-
-  const closePopover = () => {
-    const newTimeout = setTimeout(
-      () =>
-        setPopoverState(prevState => ({
-          ...prevState,
-          popoverOpen: false,
-          timeout: null
-        })),
-      hoverDelay
-    )
-    setPopoverState(prevState => ({ ...prevState, timeout: newTimeout }))
-  }
-
-  const openPopover = () => {
-    setPopoverState(prevState => {
-      if (timeout) {
-        clearTimeout(timeout)
+const LabelWithPopover = ({ children, labelContent, labelIcon }) => (
+  <div className="label-with-popover">
+    <Popover
+      bodyContent={
+        <Stack>
+          <StackItem className="close-button-spacer" />
+          <StackItem>
+            <Divider />
+          </StackItem>
+          <StackItem>{children}</StackItem>
+        </Stack>
       }
-      return { ...prevState, popoverOpen: true, timeout: null }
-    })
-  }
-
-  const shouldClose = () => {
-    const pinned = popoverOpen && !popoverPinnedOpen && !timeout
-    setPopoverState({
-      popoverOpen: pinned,
-      popoverPinnedOpen: pinned,
-      justClosed: true
-    })
-  }
-
-  const pinPopover = () => {
-    if (!justClosed) {
-      setPopoverState({
-        popoverOpen: true,
-        popoverPinnedOpen: true,
-        justClosed: false
-      })
-    } else {
-      setPopoverState(prevState => ({ ...prevState, justClosed: false }))
-    }
-  }
-
-  return (
-    <div className="label-with-popover">
-      <Popover
-        bodyContent={children}
-        isVisible={popoverOpen || popoverPinnedOpen}
-        shouldClose={shouldClose}
-        shouldOpen={pinPopover}
-        onPointerEnter={openPopover}
-        onPointerLeave={closePopover}
-        className="label-with-popover"
-        enableFlip
-        position="bottom"
-        flipBehavior={['bottom', 'top', 'right', 'left']}
-        distance={10}
-        zIndex={500}
+      className="label-with-popover"
+      enableFlip
+      position="bottom"
+      flipBehavior={['bottom', 'top', 'right', 'left']}
+      distance={10}
+      zIndex={6005}
+    >
+      <Label
+        onClick={event => {
+          event.preventDefault()
+          event.nativeEvent.preventDefault()
+          event.stopPropagation()
+        }}
+        color="grey"
+        href="#"
+        icon={labelIcon}
       >
-        <Label
-          onClick={event => {
-            event.preventDefault()
-            event.nativeEvent.preventDefault()
-            event.stopPropagation()
-            pinPopover()
-          }}
-          onPointerEnter={openPopover}
-          onPointerLeave={closePopover}
-          color="grey"
-          href="#"
-          icon={labelIcon}
-        >
-          {labelContent}
-        </Label>
-      </Popover>
-    </div>
-  )
-}
+        {labelContent}
+      </Label>
+    </Popover>
+  </div>
+)
 
 LabelWithPopover.propTypes = {
   children: PropTypes.object,

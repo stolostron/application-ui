8ac83a7dfc292f922fd57d8070fdfd8163836f03
Kevin Cormier <kcormier@redhat.com>
2020-09-17 13:57:52 -0400

    Eliminate scroll on QuerySwitcher change Make padding on refresh control smaller

--- src-web/components/common/QuerySwitcher.js
+++ src-web/components/common/QuerySwitcher.js
@@ -5,32 +5,45 @@ import React from 'react'
 import { withRouter } from 'react-router-dom'
 import queryString from 'query-string'
 import PropTypes from 'prop-types'
-import {
-  ToggleGroup,
-  ToggleGroupItem
-} from '@patternfly/react-core'
+import { ToggleGroup, ToggleGroupItem } from '@patternfly/react-core'
 
-
-const QuerySwitcher = ({ options, defaultOption, queryParam='resource', location, history }) => {
+const QuerySwitcher = ({
+  options,
+  defaultOption,
+  queryParam = 'resource',
+  location,
+  history
+}) => {
   const query = queryString.parse(location.search)
   const validOptionIds = options.map(o => o.id)
-  const isSelected = id => validOptionIds.includes(query[queryParam]) ? query[queryParam] === id : defaultOption === id
+  const isSelected = id =>
+    validOptionIds.includes(query[queryParam])
+      ? query[queryParam] === id
+      : defaultOption === id
   const handleChange = (_, event) => {
     const id = event.currentTarget.id
     query[queryParam] = id
     const newQueryString = queryString.stringify(query)
-    history.replace(`${location.pathname}${newQueryString && `?${newQueryString}`}${location.hash}`)
+    history.replace(
+      `${location.pathname}${newQueryString && `?${newQueryString}`}${
+        location.hash
+      }`,
+      { noScrollToTop: true }
+    )
   }
 
   return (
     <ToggleGroup variant="light">
-      {
-        options.map(({ id, contents }) => (
-          <ToggleGroupItem key={id} buttonId={id} isSelected={isSelected(id)} onChange={handleChange}>
-            { contents }
-          </ToggleGroupItem>
-        ))
-      }
+      {options.map(({ id, contents }) => (
+        <ToggleGroupItem
+          key={id}
+          buttonId={id}
+          isSelected={isSelected(id)}
+          onChange={handleChange}
+        >
+          {contents}
+        </ToggleGroupItem>
+      ))}
     </ToggleGroup>
   )
 }

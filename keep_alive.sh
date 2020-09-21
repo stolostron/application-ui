ebc08705b83b98c80ad57824e9111de025aebfb6
Maggie Chen <magchen@redhat.com>
2020-09-21 14:04:24 -0400

    remove namespace type from wizard (#608)
    * remove ns
    
    * remove unnecessary templates and add timeout allow
    
    * change permission
    
    * change syntax
    
    * add permission
    
    * add copyright
    
    * add permission

---
+++ keep_alive.sh
@@ -0,0 +1,9 @@
+# Copyright (c) 2020 Red Hat, Inc.
+#!/bin/bash
+
+# keep_alive.sh
+#   This script wraps a one-liner that will keep our travis worker alive by printing a message every
+#   5 minutes.  
+#
+
+while sleep 5m; do echo "\n=====[ $SECONDS seconds, still building (don't die travis, don't die)... ]=====\n"; done &

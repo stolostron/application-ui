94e8c6278d251a6c76677e69845309273636bb4b
Kevin Cormier <kcormier@redhat.com>
2020-09-22 17:04:33 -0400

    Adapt to new search-api result format

--- src-web/definitions/hcm-channel.js
+++ src-web/definitions/hcm-channel.js
@@ -102,12 +102,12 @@ export function createSubscriptionsLink(item) {
 }
 
 export function createClustersLink(item, locale) {
-  const clusterCount = R.path(['clusterCount'], item) || 0
+  const clusterCount = R.path(['clusterCount'], item) || {}
   const localPlacement = R.path(['localPlacement'], item) || false
   return getClusterCount({
     locale,
-    remoteCount: clusterCount,
-    localPlacement,
+    remoteCount: clusterCount.remoteCount,
+    localPlacement: localPlacement || clusterCount.localCount,
     name: item.name,
     namespace: item.namespace,
     kind: 'placementrule'

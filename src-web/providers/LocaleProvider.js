09bebe5a3abc68ee69563e8157286a1c2c0e552c
Kevin Cormier <kcormier@redhat.com>
2020-10-29 14:13:34 -0400

    Remove dependency on recompose

--- src-web/providers/LocaleProvider.js
+++ src-web/providers/LocaleProvider.js
@@ -6,17 +6,13 @@
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  *******************************************************************************/
 
+import React from 'react'
 import PropTypes from 'prop-types'
-import withContext from 'recompose/withContext'
-import getContext from 'recompose/getContext'
 
-const LocaleProvider = ({ children }) => children
-
-const withLocaleContext = withContext({ locale: PropTypes.string }, props => ({
-  locale: props.locale
-}))
-
-const withLocale = getContext({ locale: PropTypes.string })
-
-export { withLocale }
-export default withLocaleContext(LocaleProvider) // this can be used to wrap the root.
+export const withLocale = Component => {
+  const ComponentWithLocale = (props, context) =>
+    React.createElement(Component, { ...props, ...context })
+  ComponentWithLocale.displayName = `withLocale(${Component.displayName})`
+  ComponentWithLocale.contextTypes = { locale: PropTypes.string }
+  return ComponentWithLocale
+}

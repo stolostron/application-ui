b939a0bcb6836712fe443fdc4137b3fbb5f4c45b
jswanke <jswanke@JSWANKE-W10E.win.redhat.com>
2020-10-30 11:13:23 -0400

    Existing placement rule dropdown clears when YAML toggled on/off#6026

--- src-web/components/ApplicationCreationPage/controlData/utils.js
+++ src-web/components/ApplicationCreationPage/controlData/utils.js
@@ -391,7 +391,6 @@ export const setAvailableRules = (control, result) => {
   control.available = []
   control.availableMap = {}
   control.isLoading = false
-  control.active = ''
 
   const error = placementrules ? null : result.error
 

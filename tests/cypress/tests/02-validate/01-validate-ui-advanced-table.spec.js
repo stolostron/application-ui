7bd31bd75633bbc4a78ce3b636f540d4ec5baa7c
Valentina Birsan <vbirsan17@gmail.com>
2020-11-01 11:00:54 -0500

    allow to create more than one app per type (#756)
    * allow to create more than one app per type
    
    * update yaml to support multiple app per type
    
    Co-authored-by: Valentina Birsan <vbirsan@vbirsan@redhat.com-mac>

--- tests/cypress/tests/02-validate/01-validate-ui-advanced-table.spec.js
+++ tests/cypress/tests/02-validate/01-validate-ui-advanced-table.spec.js
@@ -7,18 +7,21 @@ import { validateAdvancedTables } from "../../views/application";
 
 describe("Application Validation Test for advanced configuration tables", () => {
   for (const type in config) {
-    const data = config[type].data;
-
-    if (data.enable) {
-      it(`Verify application channel, subscription, placement rule info from the advanced configuration tables - ${type}: ${
-        data.name
-      }`, () => {
-        validateAdvancedTables(data.name, data, type);
-      });
-    } else {
-      it(`disable validation on resource ${type}`, () => {
-        cy.log(`skipping ${type} - ${data.name}`);
-      });
-    }
+    const apps = config[type].data;
+    apps.forEach(data => {
+      if (data.enable) {
+        it(`Verify application ${
+          data.name
+        } channel, subscription, placement rule info from the advanced configuration tables - ${type}: ${
+          data.name
+        }`, () => {
+          validateAdvancedTables(data.name, data, type);
+        });
+      } else {
+        it(`disable validation on resource ${type}`, () => {
+          cy.log(`skipping ${type} - ${data.name}`);
+        });
+      }
+    });
   }
 });

f45a02736a36ec576cf74924ec8c2c27dc1a1717
Kevin Cormier <kcormier@redhat.com>
2020-11-06 15:01:45 -0500

    WIP: Fix conflicting PatternFly styles

--- webpack.config.js
+++ webpack.config.js
@@ -59,22 +59,23 @@ module.exports = {
       },
       {
         test: [/\.s?css$/],
-        exclude: {
-          and: [
-            /node_modules/,
-            {
-              or: [
-                { not: [/node_modules\/@patternfly/] },
-                {
-                  and: [
-                    /node_modules\/@open-cluster-management\/ui-components/,
-                    /node_modules\/@patternfly/
-                  ]
-                }
-              ]
-            }
-          ]
-        },
+        exclude: /node_modules/,
+        // exclude: {
+        //   and: [
+        //     /node_modules/,
+        //     {
+        //       or: [
+        //         { not: [/node_modules\/@patternfly/] },
+        //         {
+        //           and: [
+        //             /node_modules\/@open-cluster-management\/ui-components/,
+        //             /node_modules\/@patternfly/
+        //           ]
+        //         }
+        //       ]
+        //     }
+        //   ]
+        // },
         loader: ExtractTextPlugin.extract({
           fallback: "style-loader",
           use: [
@@ -116,10 +117,10 @@ module.exports = {
         include: path.resolve(__dirname, "./node_modules/monaco-editor"),
         use: ["style-loader", "css-loader"]
       },
-      // ignore styles @open-cluster-management/ui-components
+      // ignore styles under node_modules
       {
         test: /\.s?css$/,
-        include: /node_modules\/@open-cluster-management\/ui-components/,
+        include: /node_modules/, //\/@open-cluster-management\/ui-components/,
         loader: "null-loader"
       },
       {

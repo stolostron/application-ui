66a027046d3a69f5cc81e9f6b613e1f45ffd4802
John Swanke <jswanke@redhat.com>
2020-11-20 12:47:20 -0500

    build

--- webpack.config.js
+++ webpack.config.js
@@ -72,7 +72,7 @@ module.exports = {
               path.resolve(__dirname, './node_modules'),
             ],
             exclude: [
-              //path.resolve(__dirname, './node_modules/@open-cluster-management/temptifly'),
+              path.resolve(__dirname, './node_modules/@open-cluster-management/temptifly'),
             ]
           }
         ],

c8f5af521782b2741d05a4e4308b1daf5acf844c
Valentina Birsan <vbirsan@vbirsan@redhat.com-mac>
2021-01-12 17:23:06 -0500

    move ansible op

--- tests/cypress/support/index.js
+++ tests/cypress/support/index.js
@@ -48,8 +48,8 @@ before(() => {
     cy.acquireToken().then(token => {
       Cypress.env("token", token);
     });
+    cy.installAnsibleOperator();
   }
-  cy.installAnsibleOperator();
 });
 
 Cypress.on("uncaught:exception", (err, runnable) => {

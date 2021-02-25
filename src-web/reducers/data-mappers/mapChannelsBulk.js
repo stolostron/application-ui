0333e0ca8fb21fc70a10ec4192969a39e41e7679
Kevin Cormier <kcormier@redhat.com>
2021-02-25 10:28:38 -0500

    Fix spacing

--- src-web/reducers/data-mappers/mapChannelsBulk.js
+++ src-web/reducers/data-mappers/mapChannelsBulk.js
@@ -5,7 +5,7 @@
  * US Government Users Restricted Rights - Use, duplication or disclosure
  * restricted by GSA ADP Schedule Contract with IBM Corp.
  *******************************************************************************/
-//  Copyright (c) 2020 Red Hat, Inc.
+// Copyright (c) 2020 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
 
 // @flow

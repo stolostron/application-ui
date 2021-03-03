e6c8132fc330d3382e633c25b8696e52b0bb76f5
Valentina Birsan <vbirsan@redhat.com>
2021-03-03 10:46:19 -0500

    add window tests

--- tests/jest/components/CreateApplicationPage/components/TimeWindow.test.js
+++ tests/jest/components/CreateApplicationPage/components/TimeWindow.test.js
@@ -15,38 +15,63 @@ import React from "react";
 import { TimeWindow } from "../../../../../src-web/components/ApplicationCreationPage/components/TimeWindow";
 import renderer from "react-test-renderer";
 import _ from "lodash";
-import { mount } from "enzyme";
+import { shallow } from "enzyme";
 
-const control = {
-  active: {}
+const controlNoWindow = {
+  active: {
+    days: [],
+    mode: "",
+    showTimeSection: false,
+    timeList: [{ id: 0, start: "", end: "", validTime: true }],
+    timeListID: 1,
+    timezone: ""
+  }
 };
 
-describe("TimeWindow component", () => {
-  it("renders as expected", () => {
+const controlActive = {
+  active: {
+    days: [],
+    mode: "active",
+    showTimeSection: false,
+    timeList: [{ id: 0, start: "", end: "", validTime: true }],
+    timeListID: 1,
+    timezone: ""
+  }
+};
+
+const handleChange = jest.fn((checked, event) => {
+  return null;
+});
+
+describe("TimeWindow component default, no-window selected", () => {
+  it("basic UI snapshot validation", () => {
     const component = renderer.create(
       <TimeWindow
         locale="en-US"
         type="custom"
         available={[]}
-        control={control}
+        control={controlNoWindow}
+        handleChange={handleChange}
       />
     );
     expect(component.toJSON()).toMatchSnapshot();
   });
 });
 
-describe("on control change function", () => {
-  it("renders as expected", () => {
-    const wrapper = mount(
+describe("TimeWindow component, active window selected", () => {
+  it("active window components coverage", () => {
+    const wrapper = shallow(
       <TimeWindow
         locale="en-US"
         type="custom"
         available={[]}
-        control={control}
+        control={controlActive}
+        handleChange={handleChange}
       />
     );
     const evt = {
       target: {
+        name: "timeWindow-mode-container",
         value: "value-testing"
       },
       selectedItems: ["selectedItems-testing-1", "selectedItems-testing-2"]
@@ -74,9 +99,60 @@ describe("on control change function", () => {
       .at(0)
       .simulate("click", evt);
 
+    const evtDaySelector = {
+      target: {
+        name: "days-selector",
+        value: "value-testing",
+        id: "day-0-input"
+      },
+      selectedItems: ["selectedItems-testing-1", "selectedItems-testing-2"]
+    };
+
+    wrapper
+      .find("#active-mode-undefined")
+      .at(0)
+      .simulate("change", true, evt);
+
+    wrapper
+      .find(".config-timezone-combo-box")
+      .at(0)
+      .simulate("change");
+    wrapper
+      .find(".config-timezone-combo-box")
+      .at(0)
+      .simulate("filter", evt);
+
     wrapper
       .find("#tue-undefined")
       .at(0)
-      .simulate("click", evt);
+      .simulate("change", true, evt);
+
+    wrapper
+      .find("#tue-undefined")
+      .at(0)
+      .simulate("change", true, evtDaySelector);
+
+    wrapper
+      .find("#tue-undefined")
+      .at(0)
+      .simulate("change", false, evtDaySelector);
+
+    //start-time-0-input
+    wrapper.find("#start-time-0").simulate("click", true, evt);
+
+    const evtTimeSelector = {
+      target: {
+        name: "time-selector",
+        value: "11:20",
+        id: "start-time-0-input"
+      }
+    };
+    //pf-m-clock
+    wrapper
+      .find("TimePicker")
+      .at(0)
+      .simulate("change", "11:20", evtTimeSelector);
+
+    wrapper.find(".add-time-btn").simulate("click", true, evtTimeSelector);
   });
 });

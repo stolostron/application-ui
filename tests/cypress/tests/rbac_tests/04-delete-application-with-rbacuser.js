051d3fc9b459c98fea2fe90b47aecd218eec3772
Valentina Birsan <vbirsan@redhat.com>
2021-03-04 16:00:17 -0500

    fix placement leak on RBAC tests

--- tests/cypress/tests/rbac_tests/04-delete-application-with-rbacuser.js
+++ tests/cypress/tests/rbac_tests/04-delete-application-with-rbacuser.js
@@ -7,16 +7,14 @@ import {
   verifyUnauthorizedApplicationDelete
 } from "../../views/application";
 import {
+  getManagedClusterName,
   channelsInformation,
   deleteChannel,
-  deleteNamespaceHub,
-  getManagedClusterName
+  deleteNamespaceHub
 } from "../../views/resources";
-import { resourceTable, modal, getResourceKey } from "../../views/common";
 
 const mngdTestAdminRoles = "admin-managed-cluster";
 const mngdTestViewRole = "view-managed-cluster";
-const mngdTestEditRole = "edit-managed-cluster";
 
 describe("Application UI: [P1][Sev1][app-lifecycle-ui][RBAC] Delete application Test", () => {
   if (Cypress.env("RBAC_TEST")) {
@@ -24,33 +22,49 @@ describe("Application UI: [P1][Sev1][app-lifecycle-ui][RBAC] Delete application 
       if (type == "git") {
         const apps = config[type].data;
         apps.forEach(data => {
-          if (data.enable) {
+          if (data.enable && data.config.length > 1) {
             const name = data.name;
             it(`get the name of the managed OCP cluster`, () => {
               getManagedClusterName();
             });
 
-            it(`[P1][Sev1][app-lifecycle-ui][RBAC] Verify a user with role ${mngdTestViewRole} cannot delete 
+            it(`[app-lifecycle-ui][RBAC] Verify a user with role ${mngdTestViewRole} cannot delete 
         application ${data.name} from UI`, () => {
               const namespace = Cypress.env("managedCluster");
-              // cy.logInAsRole(mngdTestViewRole)
               cy.rbacSwitchUser(mngdTestViewRole);
               verifyUnauthorizedApplicationDelete(name, namespace);
             });
 
-            it(`[P1][Sev1][app-lifecycle-ui][RBAC] Verify application ${
+            it(`Try to delete channel with insecureSkipVerify option for application ${
+              data.name
+            }`, () => {
+              const key = 2; // our tests use the invalidate option on add new subscription, which has index 2
+              const name = data.name;
+              const namespace = Cypress.env("managedCluster");
+              cy.log(`DATA ${name}`);
+              channelsInformation(name, key, namespace).then(
+                ({ channelNs, channelName }) => {
+                  cy.log(`CHANNEL ${channelName}, ${channelNs}`);
+                  if (channelName.indexOf("insecureskipverifyoption") != -1) {
+                    cy.log(
+                      `delete channel ${channelName} ns:  ${channelNs}, set for insecureSkipVerifyOption test `
+                    );
+                    deleteChannel(channelName, channelNs);
+
+                    cy.log(
+                      `Delete insecureSkipVerify channel namespace, ${channelNs}`
+                    );
+                    deleteNamespaceHub(data, channelName, type);
+                  }
+                }
+              );
+            });
+            it(`[app-lifecycle-ui][RBAC] Verify application ${
               data.name
             } is deleted from UI with role: ${mngdTestAdminRoles} `, () => {
-              // cy.logInAsRole(mngdTestAdminRoles)
               cy.rbacSwitchUser(mngdTestAdminRoles);
               const namespace = Cypress.env("managedCluster");
               deleteApplicationUI(data.name, namespace);
-            });
-          } else {
-            it(`Verify disable deletion on resource ${
-              data.name
-            } ${type}`, () => {
-              cy.log(`skipping ${type} - ${data.name}`);
             });
           }
         });

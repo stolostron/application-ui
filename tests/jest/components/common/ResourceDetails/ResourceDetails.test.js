79f07b57a08e3baf947583b40a19aec5a0b1a8a7
chenz4027 <magchen@redhat.com>
2021-03-19 14:21:13 -0400

    update jest test

--- tests/jest/components/common/ResourceDetails/ResourceDetails.test.js
+++ tests/jest/components/common/ResourceDetails/ResourceDetails.test.js
@@ -40,6 +40,16 @@ jest.mock("../../../../../lib/client/access-helper.js", () => ({
       }
     };
     return Promise.resolve(data);
+  }),
+  getArgoRoute: jest.fn(() => {
+    const data = {
+      data: {
+        argoRoute: {
+          "test-ns": "http://argocd.fakeurl.com"
+        }
+      }
+    };
+    return Promise.resolve(data);
   })
 }));
 

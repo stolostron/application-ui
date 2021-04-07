18acafde08bd20087f4e6ef326fb3fcfcde5d7e5
chenz4027 <magchen@redhat.com>
2021-04-07 15:25:21 -0400

    integrate the common header

--- controllers/ui/app.js
+++ controllers/ui/app.js
@@ -10,117 +10,40 @@
 
 'use strict'
 
-const ReactDOMServer = require('react-dom/server'),
-      thunkMiddleware = require('redux-thunk').default,
-      redux = require('redux'),
-      React = require('react'),
-      express = require('express'),
-      StaticRouter = require('react-router-dom').StaticRouter,
+const express = require('express'),
       context = require('../../lib/shared/context'),
       msgs = require('../../nls/platform.properties'),
       config = require('../../config'),
-      cookieUtil = require('../../lib/server/cookie-util'),
+      // cookieUtil = require('../../lib/server/cookie-util'),
       appUtil = require('../../lib/server/app-util'),
-      Provider = require('react-redux').Provider,
-      router = express.Router({ mergeParams: true }),
-      lodash = require('lodash'),
-      headerClient = require('../../lib/server/header-client')
+      router = express.Router({ mergeParams: true })
 
-var log4js = require('log4js'),
-    logger = log4js.getLogger('app')
-
-let App, Login, role, reducers //laziy initialize to reduce startup time seen on k8s
-router.get('/logout', (req, res) => {
-  var LOGOUT_API = '/v1/auth/logout'
-  var callbackUrl = req.headers['host']
-  cookieUtil.deleteAuthCookies(res)
-  logger.debug('headers host:' + callbackUrl)
-  var redirectUrl =
-    process.env.NODE_ENV !== 'development' && callbackUrl
-      ? `https://${callbackUrl}${LOGOUT_API}`
-      : `${config.get('headerUrl')}${LOGOUT_API}`
-  logger.debug('Final logout url:' + redirectUrl)
-  return res.send({ redirectUrl })
-})
+// router.get('/logout', (req, res) => {
+//   var LOGOUT_API = '/v1/auth/logout'
+//   var callbackUrl = req.headers['host']
+//   cookieUtil.deleteAuthCookies(res)
+//   logger.debug('headers host:' + callbackUrl)
+//   var redirectUrl =
+//     process.env.NODE_ENV !== 'development' && callbackUrl
+//       ? `https://${callbackUrl}${LOGOUT_API}`
+//       : `${config.get('headerUrl')}${LOGOUT_API}`
+//   logger.debug('Final logout url:' + redirectUrl)
+//   return res.send({ redirectUrl })
+// })
 
 router.get('*', (req, res) => {
-  reducers =
-    reducers === undefined ? require('../../src-web/reducers') : reducers
-
-  const store = redux.createStore(
-    redux.combineReducers(reducers),
-    redux.applyMiddleware(
-      thunkMiddleware // lets us dispatch() functions
+  const ctx = getContext(req)
+  return res.render(
+    'home',
+    Object.assign(
+      {
+        manifest: appUtil.app().locals.manifest,
+        contextPath: config.get('contextPath'),
+        props: ctx
+      },
+      ctx
     )
   )
-
-  Login = Login === undefined ? require('../../src-web/actions/login') : Login
-  store.dispatch(Login.receiveLoginSuccess(req.user))
-
-  App =
-    App === undefined ? require('../../src-web/containers/App').default : App
-  const context = getContext(req)
-
-  headerClient.getHeader(req, (err, headerRes) => {
-    if (err) return res.status(500).send(err)
-
-    const {
-      headerHtml: header,
-      props: propsH,
-      state: stateH,
-      files: filesH
-    } = headerRes
-
-    if (!header || !propsH || !stateH || !filesH) {
-      logger.err(headerRes.body)
-      return res.status(500).send(headerRes.body)
-    }
-
-    role = role === undefined ? require('../../src-web/actions/role') : role
-    if (stateH.role) {
-      store.dispatch(role.roleReceiveSuccess(stateH.role.role))
-    }
-
-    if (process.env.NODE_ENV === 'development') {
-      lodash.forOwn(filesH, value => {
-        value.path = `${config.get('contextPath')}/api/proxy${value.path}` //preprend with proxy route
-      })
-    }
-
-    const serverProps = {
-      ...context
-    }
-
-    try {
-      res.render(
-        'home',
-        Object.assign(
-          {
-            manifest: appUtil.app().locals.manifest,
-            content: ReactDOMServer.renderToString(
-              <Provider store={store}>
-                <StaticRouter location={req.originalUrl} context={context}>
-                  <App />
-                </StaticRouter>
-              </Provider>
-            ),
-            contextPath: config.get('contextPath'),
-            headerContextPath: config.get('headerContextPath'),
-            state: store.getState(),
-            props: serverProps,
-            header: header,
-            propsH: propsH,
-            stateH: stateH,
-            filesH: filesH
-          },
-          context
-        )
-      )
-    } catch (e) {
-      //eslint-disable-next-line
-      console.error(e);
-    }
-  })
 })
 
 function getContext(req) {

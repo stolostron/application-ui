b46a1875e07b2828deea75f7ba4aca0735af1f36
John Swanke <jswanke@redhat.com>
2021-04-21 13:20:23 -0400

    test
    Signed-off-by: U-WIN\jswanke <jswanke@JSWANKE-W10E.win.redhat.com>

--- src-web/components/ApplicationCreationPage/controlData/ControlData.js
+++ src-web/components/ApplicationCreationPage/controlData/ControlData.js
@@ -4,7 +4,7 @@
  *
  * Note to U.S. Government Users Restricted Rights:
  * Use, duplication or disclosure restricted by GSA ADP Schedule
- * Contract with IBM Corp.
+ * Contract  with IBM Corp.
  ****************************************************************************** */
 // Copyright (c) 2020 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project

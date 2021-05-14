8bfb07babd3bdbe3c934cbfd703b27c3b6bc9efc
sahare <sahare@redhat.com>
2021-05-14 14:43:18 -0400

    Remove manual refresh annotation from yaml editor
    Signed-off-by: sahare <sahare@redhat.com>

--- src-web/components/ApplicationCreationPage/transformers/transform-data-to-resources.js
+++ src-web/components/ApplicationCreationPage/transformers/transform-data-to-resources.js
@@ -35,7 +35,6 @@ const keepKeys = [
   'apps.open-cluster-management.io/git-tag',
   'apps.open-cluster-management.io/git-clone-depth',
   'apps.open-cluster-management.io/reconcile-rate',
-  'apps.open-cluster-management.io/manual-refresh-time',
   'apps.open-cluster-management.io/deployables' //needed by the topology to show deployable information
 ]
 

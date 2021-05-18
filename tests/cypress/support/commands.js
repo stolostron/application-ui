92dd65b7347a2f53df6c46704191100cd7c10f87
Valentina Birsan <vbirsan@redhat.com>
2021-05-18 09:45:27 -0400

    clean up cookie on login
    Signed-off-by: Valentina Birsan <vbirsan@redhat.com>

--- tests/cypress/support/commands.js
+++ tests/cypress/support/commands.js
@@ -38,6 +38,39 @@ const authUrl = Cypress.config().baseUrl.replace(
 
 Cypress.Commands.add("login", (idp, user, password) => {
   cy.log(`Attempt to log in app tests user ${user} with idp ${idp}`);
+  const APIServer = Cypress.env("OC_CLUSTER_URL");
+  cy
+    .clearCookies() // clear cookies so we do login again
+
+    .then(() => {
+      if (!Cypress.config().baseUrl.includes("localhost")) {
+        expect(APIServer).to.not.equal(undefined);
+        // check who is the current user
+        cy
+          .exec("oc whoami", { failOnNonZeroExit: false })
+          .then(res => {
+            const currentUser = res.stdout.replace("kube:admin", "kubeadmin");
+            cy.log(`Currently logged to 'oc' as ${currentUser}`);
+            if (currentUser != user || force) {
+              // do oc login as the required user
+              cy.log(`Doing 'oc login' as ${user}`);
+              cy
+                .exec(
+                  `oc login --server=${APIServer} -u ${user} -p ${password}`
+                )
+                .then(res => cy.log(res.stdout));
+            }
+          })
+          .then(() => {
+            cy.exec("oc whoami -t").then(res => {
+              // get token and set cookie and env var accordingly
+              Cypress.env("token", res.stdout);
+              cy.setCookie("acm-access-token-cookie", Cypress.env("token"));
+            });
+          });
+      }
+    });
+
   cy
     .get(".pf-c-login__main-body")
     .get(".pf-c-button")
@@ -73,8 +106,18 @@ Cypress.Commands.add("logout", () => {
       //logout when test starts since we need to use the app idp user
       cy.log("Logging out existing user");
       cy.get($btn).click();
-      cy.contains("Logout").click();
-      // cy.clearCookies()
+      if (Cypress.config().baseUrl.includes("localhost")) {
+        cy
+          .contains("Logout")
+          .click()
+          .clearCookies();
+      } else {
+        cy.contains("Logout").click();
+        cy
+          .location("pathname")
+          .should("match", new RegExp("/oauth/authorize(\\?.*)?$"))
+          .clearCookies();
+      }
     });
 });
 

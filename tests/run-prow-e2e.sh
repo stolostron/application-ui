0cf95c3081f8ab1e130aa1d02c56685b791ac814
chenz4027 <magchen@redhat.com>
2021-06-04 11:35:55 -0400

    refactor unneeded sections
    Signed-off-by: chenz4027 <magchen@redhat.com>

--- tests/run-prow-e2e.sh
+++ tests/run-prow-e2e.sh
@@ -4,27 +4,6 @@
 
 set -e
 
-UI_CURRENT_IMAGE=$1
-
-
-###############################################################################
-# Install oc and Cluster Keeper
-###############################################################################
-echo "Install oc and Cluster Keeper"
-
-echo "Running oc/install..."
-# make oc/install
-echo "Cloning Cluster Keeper"
-git clone https://github.com/open-cluster-management/cluster-keeper.git
-pushd cluster-keeper
-cat > user.env << EOF
-CLUSTERPOOL_CLUSTER=$CLUSTERPOOL_HOST_API
-CLUSTERPOOL_TARGET_NAMESPACE=$CLUSTERPOOL_HOST_NAMESPACE
-CLUSTERCLAIM_GROUP_NAME=$CLUSTERPOOL_GROUP_NAME
-EOF
-export PATH=${PATH}:$(pwd)
-popd
-
 ###############################################################################
 # Test Setup
 ###############################################################################
@@ -51,17 +30,8 @@ export CYPRESS_MANAGED_OCP_URL=$(echo $MANAGED_CREDS | jq -r '.api_url')
 export CYPRESS_MANAGED_OCP_USER=$(echo $MANAGED_CREDS | jq -r '.username')
 export CYPRESS_MANAGED_OCP_PASS=$(echo $MANAGED_CREDS | jq -r '.password')
 
-
-echo "Running oc/install..."
-make oc/install
 echo "Running oc/login..."
 make oc/login
 
-echo "Running pull-test-image..."
-make pull-test-image
-
-# Use setup script to set variables
-. ./setup-env.sh > /dev/null
-
 echo "Functional Tests"
-make run-test-image-pr
+./start-cypress-tests.sh

263b5831e6dc538aa3a2b73fe2ccc59290cb7788
chenz4027 <magchen@redhat.com>
2021-06-16 11:55:03 -0400

    fix compile issue
    Signed-off-by: chenz4027 <magchen@redhat.com>

--- tests/cypress/tests/01-force-failure/force-faiure.js
+++ tests/cypress/tests/01-force-failure/force-faiure.js
@@ -1,8 +1,5 @@
 // Copyright (c) 2020 Red Hat, Inc.
 // Copyright Contributors to the Open Cluster Management project
-
-import { testInvalidApplicationInput } from "../../views/common";
-
 describe("Application UI: [P2][Sev2][app-lifecycle-ui] Application Creation Validate invalid input Test", () => {
   it(`force failure to trigger slack`, () => {
     cy.get("#force-failure");

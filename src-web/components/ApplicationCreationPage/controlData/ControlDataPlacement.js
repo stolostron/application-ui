65642fdc17320daf02f63ca1ca4edd28824577f9
randybrunopiverger <rbrunopi@redhat.com>
2022-05-10 10:00:16 -0700

    unneeded method
    Signed-off-by: randybrunopiverger <rbrunopi@redhat.com>

--- src-web/components/ApplicationCreationPage/controlData/ControlDataPlacement.js
+++ src-web/components/ApplicationCreationPage/controlData/ControlDataPlacement.js
@@ -220,20 +220,6 @@ export const reverseExistingRule = (control, templateObject) => {
   }
   return control
 }
-export const reverseExistingRuleName = (control, templateObject) => {
-  const active = _.get(
-    templateObject,
-    getSourcePath('Subscription[0].spec.placement.placementRef.name')
-  )
-  if (active && control.active === undefined) {
-    const { groupControlData } = control
-    const selectedRuleName = groupControlData.find(
-      ({ id }) => id === 'selectedRuleName'
-    )
-    selectedRuleName.active = active.$v
-  }
-  return control
-}
 
 export const reverseOnline = (control, templateObject) => {
   const active = _.get(
@@ -320,7 +306,7 @@ const placementData = async () => [
   {
     id: 'selectedRuleName',
     type: 'hidden',
-    reverse: reverseExistingRuleName
+    reverse: reverseExistingRule
   },
   {
     type: 'custom',
